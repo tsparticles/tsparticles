9aeef4217793afa64eaadbceecd7972b1ff4a4c7
Matteo Bruni <ar3s@icloud.com>
2020-02-16 01:09:43 +0100

    fix

--- src/utils/constants.ts
+++ src/utils/constants.ts
@@ -48,7 +48,7 @@ export class Constants {
                     distance: 200,
                     duration: 0.4,
                 },
-            }
+            },
         },
         particles: {
             color: {
