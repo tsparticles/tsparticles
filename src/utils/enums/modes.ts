84d31bb026458c6f440a05461d08cda412acbfe3
Matteo Bruni <ar3s@icloud.com>
2020-02-16 16:21:54 +0100

    fix

--- src/utils/enums/modes.ts
+++ src/utils/enums/modes.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 export enum ClickMode {
     bubble = "bubble",
--- src/utils/enums/types.ts
+++ src/utils/enums/types.ts
@@ -1,4 +1,4 @@
-'use strict';
+"use strict";
 
 export enum ProcessBubbleType {
     opacity = "opacity",
