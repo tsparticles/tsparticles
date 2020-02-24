0c887278438483a6698241b4d157ca30eb018756
Matteo Bruni <ar3s@icloud.com>
2020-02-24 11:55:33 +0100

    code style

--- support/support.d.ts
+++ support/support.d.ts
@@ -2,9 +2,9 @@ import {IOptions} from "../src/Interfaces/IOptions";
 import {Container} from "../src/Classes/Container";
 
 declare class ParticlesJS {
-    static load(tagId: string, params: IOptions): Container;
+    public static load(tagId: string, params: IOptions): Container;
 
-    static loadJson(tagId: string, pathConfigJson: string, callback: ((container: Container) => void)): void;
+    public static loadJson(tagId: string, pathConfigJson: string, callback: ((container: Container) => void)): void;
 
-    static setOnClickHandler(callback: EventListenerOrEventListenerObject): void;
+    public static setOnClickHandler(callback: EventListenerOrEventListenerObject): void;
 }
