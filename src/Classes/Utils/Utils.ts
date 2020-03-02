8983967efaeb81b2e7c58772d9bf212fc2a3f486
Matteo Bruni <ar3s@icloud.com>
2020-03-02 14:52:31 +0100

    code style

--- src/Classes/Utils/Utils.ts
+++ src/Classes/Utils/Utils.ts
@@ -50,15 +50,6 @@ export class Utils {
         return result;
     }
 
-    private static hue2rgb(p: number, q: number, t: number) {
-        if (t < 0) t += 1;
-        if (t > 1) t -= 1;
-        if (t < 1 / 6) return p + (q - p) * 6 * t;
-        if (t < 1 / 2) return q;
-        if (t < 2 / 3) return p + (q - p) * (2 / 3 - t) * 6;
-        return p;
-    }
-
     /**
      * Generate a random RGBA color
      */
@@ -170,5 +161,29 @@ export class Utils {
         }
 
         return res;
+    }
+
+    private static hue2rgb(p: number, q: number, t: number) {
+        if (t < 0) {
+            t += 1;
+        }
+
+        if (t > 1) {
+            t -= 1;
+        }
+
+        if (t < 1 / 6) {
+            return p + (q - p) * 6 * t;
+        }
+
+        if (t < 1 / 2) {
+            return q;
+        }
+
+        if (t < 2 / 3) {
+            return p + (q - p) * (2 / 3 - t) * 6;
+        }
+
+        return p;
     }
 }
