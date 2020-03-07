6453ec2cbc2e0a971c67a571847c9c836220b041
Matteo Bruni <ar3s@icloud.com>
2020-03-08 00:41:42 +0100

    console eslint

--- src/Classes/Utils/Messages.ts
+++ src/Classes/Utils/Messages.ts
@@ -1,10 +1,10 @@
+/* eslint no-console: ["error", { allow: ["warn", "error"] }] */
+
 export class Messages {
     public static deprecated(oldProperty: string, newProperty: string): void {
-        /* eslint no-console: "error" */
         if (console) {
             const obsolete =`The property ${oldProperty} is obsolete and will be removed in a future release.`;
             const useNew = `Please use the new property ${newProperty}.`;
-            /* eslint no-console: "error" */
             console.warn(`${obsolete} ${useNew}`);
         }
     }
