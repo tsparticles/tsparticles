273cfda0faf6a09a6a7f8b501456aca4b69fae84
Matteo Bruni <ar3s@icloud.com>
2020-03-20 14:31:00 +0100

    fixed bug in 1.10.3

--- src/Main.ts
+++ src/Main.ts
@@ -6,7 +6,7 @@
 /* Demo / Generator : https://particles.matteobruni.it/
 /* GitHub : https://www.github.com/matteobruni/tsparticles
 /* How to use? : Check the GitHub README
-/* v1.10.3
+/* v1.10.4
 /* ----------------------------------------------- */
 import {Container} from "./Classes/Container";
 import {Loader} from "./Classes/Loader";
