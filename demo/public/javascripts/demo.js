13c23271d4be1abd2c60c0ae548a5d1914f986af
Matteo Bruni <ar3s@icloud.com>
2020-03-26 00:00:57 +0100

    demo renewed, fixed export config and image on container.ts

--- demo/public/javascripts/demo.js
+++ demo/public/javascripts/demo.js
@@ -1,25 +1,26 @@
 (function () {
-    let otherLoads;
-
-    if (window.onload) {
-        otherLoads = window.onload;
-    }
-
     const stats = new Stats();
 
     stats.setMode(0);
     stats.domElement.style.position = "absolute";
-    stats.domElement.style.left = "0px";
-    stats.domElement.style.top = "0px";
+    stats.domElement.style.left = "3px";
+    stats.domElement.style.top = "3px";
+    stats.domElement.id = "stats-graph"
 
-    let updateStats = function () {
+    let initStats = function () {
         const count_particles = document.querySelector(".js-count-particles");
         const update = function () {
             stats.begin();
             stats.end();
 
-            count_particles.innerText = tsParticles.domItem(0).particles.count;
-            
+            const container = tsParticles.domItem(0);
+
+            if (container) {
+                count_particles.innerText = container.particles.count;
+            } else {
+                count_particles.innerText = 0;
+            }
+
             requestAnimationFrame(update);
         };
 
@@ -57,15 +58,44 @@
             editor.set(particles.options);
             editor.expandAll();
             updateBackground();
-            updateStats();
         });
     };
 
-    window.onload = function () {
-        if (otherLoads) {
-            otherLoads();
+    let initSidebar = function () {
+        const rightCaret = document.body.querySelector('.caret-right');
+        const leftCaret = document.body.querySelector('.caret-left');
+        const sidebar = document.getElementById('sidebar');
+        const sidebarHidden = sidebar.hasAttribute('hidden');
+
+        if (sidebarHidden) {
+            leftCaret.setAttribute('hidden', '');
+            rightCaret.removeAttribute('hidden');
+        } else {
+            rightCaret.setAttribute('hidden', '');
+            leftCaret.removeAttribute('hidden');
+        }
+    };
+
+    let toggleSidebar = function () {
+        const rightCaret = document.body.querySelector('.caret-right');
+        const leftCaret = document.body.querySelector('.caret-left');
+        const sidebar = document.getElementById('sidebar');
+        const sidebarHidden = sidebar.hasAttribute('hidden');
+
+        if (sidebarHidden) {
+            rightCaret.setAttribute('hidden', '');
+            leftCaret.removeAttribute('hidden');
+            sidebar.removeAttribute('hidden');
+        } else {
+            leftCaret.setAttribute('hidden', '');
+            rightCaret.removeAttribute('hidden');
+            sidebar.setAttribute('hidden', '');
         }
 
+        tsParticles.domItem(0).refresh();
+    };
+
+    window.addEventListener('load', function () {
         const element = document.getElementById('editor');
         const options = {
             mode: 'tree',
@@ -111,6 +141,74 @@
             });
         };
 
-        document.body.querySelector('#tsparticles-container').appendChild(stats.domElement);
-    };
+        document.body.querySelector('#stats').appendChild(stats.domElement);
+
+        const statsToggler = document.body.querySelector('#toggle-stats');
+
+        statsToggler.addEventListener('click', function () {
+            const statsEl = document.body.querySelector('#stats');
+            if (statsEl.hasAttribute('hidden')) {
+                statsEl.removeAttribute('hidden');
+            } else {
+                statsEl.setAttribute('hidden', '');
+            }
+        });
+
+        const sidebarToggler = document.body.querySelector('.toggle-sidebar');
+
+        sidebarToggler.addEventListener('click', function () {
+            toggleSidebar();
+        });
+
+        document.getElementById('export-image').addEventListener('click', function () {
+            const container = tsParticles.domItem(0);
+
+            if (container) {
+                container.exportImage(function (blob) {
+                    console.log(blob);
+                    const modalBody = document.body.querySelector('#exportModal .modal-body .modal-body-content');
+                    const particlesContainer = document.getElementById('tsparticles');
+
+                    modalBody.innerHTML = '';
+                    modalBody.style.backgroundColor = particlesContainer.style.backgroundColor;
+                    modalBody.style.backgroundImage = particlesContainer.style.backgroundImage;
+                    modalBody.style.backgroundPosition = particlesContainer.style.backgroundPosition;
+                    modalBody.style.backgroundRepeat = particlesContainer.style.backgroundRepeat;
+                    modalBody.style.backgroundSize = particlesContainer.style.backgroundSize;
+
+                    console.log(modalBody.style);
+
+                    const image = new Image();
+
+                    image.className = 'img-fluid';
+                    image.onload = () => URL.revokeObjectURL(image.src);
+
+                    const url = URL.createObjectURL(blob);
+
+                    image.src = url;
+
+                    console.log(url);
+
+                    modalBody.appendChild(image);
+
+                    $('#exportModal').modal('show');
+                });
+            }
+        });
+
+        document.getElementById('export-config').addEventListener('click', function () {
+            const container = tsParticles.domItem(0);
+
+            if (container) {
+                const modalBody = document.body.querySelector('#exportModal .modal-body .modal-body-content');
+
+                modalBody.innerHTML = `<pre>${container.exportConfiguration()}</pre>`;
+
+                $('#exportModal').modal('show');
+            }
+        });
+
+        initSidebar();
+        initStats();
+    });
 })();
