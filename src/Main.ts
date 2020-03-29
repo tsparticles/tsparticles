c3c4c0fafa497a1cdc671736bf23975031369578
Matteo Bruni <ar3s@icloud.com>
2020-03-29 22:58:43 +0200

    version bump

--- src/Main.ts
+++ src/Main.ts
@@ -4,7 +4,7 @@
 /* Demo / Generator : https://particles.matteobruni.it/
 /* GitHub : https://www.github.com/matteobruni/tsparticles
 /* How to use? : Check the GitHub README
-/* v1.11.1
+/* v1.11.2
 /* ----------------------------------------------- */
 import type { Container } from "./Classes/Container";
 import { Loader } from "./Classes/Loader";
