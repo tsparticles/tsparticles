deade67cefb6e6801921322ebae4c4283c61d20e
Matteo Bruni <ar3s@icloud.com>
2020-03-30 23:33:23 +0200

    preparing vue version

--- projects/particles.vue/src/App.vue
+++ projects/particles.vue/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+    <Particles id="tsparticles" />
   </div>
 </template>
 
--- projects/particles.vue/src/components/Particles.vue
+++ projects/particles.vue/src/components/Particles.vue
@@ -1,114 +1,212 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br />
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
-        >vue-cli documentation</a
-      >.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel"
-          target="_blank"
-          rel="noopener"
-          >babel</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-typescript"
-          target="_blank"
-          rel="noopener"
-          >typescript</a
-        >
-      </li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank" rel="noopener"
-          >Forum</a
-        >
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank" rel="noopener"
-          >Community Chat</a
-        >
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank" rel="noopener"
-          >Twitter</a
-        >
-      </li>
-      <li>
-        <a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a>
-      </li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li>
-        <a href="https://router.vuejs.org" target="_blank" rel="noopener"
-          >vue-router</a
-        >
-      </li>
-      <li>
-        <a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a>
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/vue-devtools#vue-devtools"
-          target="_blank"
-          rel="noopener"
-          >vue-devtools</a
-        >
-      </li>
-      <li>
-        <a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener"
-          >vue-loader</a
-        >
-      </li>
-      <li>
-        <a
-          href="https://github.com/vuejs/awesome-vue"
-          target="_blank"
-          rel="noopener"
-          >awesome-vue</a
-        >
-      </li>
-    </ul>
-  </div>
+  <div
+    :id="id"
+    :clickEffect="clickEffect"
+    :clickMode="clickMode"
+    :color="color"
+    :hoverEffect="hoverEffect"
+    :hoverMode="hoverMode"
+    :lineLinked="lineLinked"
+    :linesColor="linesColor"
+    :linesDistance="linesDistance"
+    :linesOpacity="linesOpacity"
+    :linesWidth="linesWidth"
+    :moveSpeed="moveSpeed"
+    :particleOpacity="particleOpacity"
+    :particleSize="particleSize"
+    :particlesNumber="particlesNumber"
+    :shapeType="shapeType"
+  ></div>
 </template>
 
 <script lang="ts">
 import { Component, Prop, Vue } from "vue-property-decorator";
+import { ClickMode } from "tsparticles/dist/Enums/Modes/ClickMode";
+import { HoverMode } from "tsparticles/dist/Enums/Modes/HoverMode";
+import { ShapeType } from "tsparticles/dist/Enums/ShapeType";
+import { Loader } from "tsparticles/dist/Classes/Loader";
 
 @Component
 export default class Particles extends Vue {
-  @Prop() private msg!: string;
+  @Prop() private id!: string;
+  @Prop() private clickEffect!: boolean;
+  @Prop() private clickMode!: ClickMode;
+  @Prop() private color!: string;
+  @Prop() private hoverEffect!: boolean;
+  @Prop() private hoverMode!: HoverMode;
+  @Prop() private lineLinked!: boolean;
+  @Prop() private linesColor!: string;
+  @Prop() private linesDistance!: number;
+  @Prop() private linesOpacity!: number;
+  @Prop() private linesWidth!: number;
+  @Prop() private moveSpeed!: number;
+  @Prop() private particleOpacity!: number;
+  @Prop() private particleSize!: number;
+  @Prop() private particlesNumber!: number;
+  @Prop() private shapeType!: ShapeType;
+
+  private mounted(): void {
+    this.$nextTick(() => {
+      this.initTsParticles(
+        this.color,
+        this.particleOpacity,
+        this.particlesNumber,
+        this.shapeType,
+        this.particleSize,
+        this.linesColor,
+        this.linesWidth,
+        this.lineLinked,
+        this.linesOpacity,
+        this.linesDistance,
+        this.moveSpeed,
+        this.hoverEffect,
+        this.hoverMode,
+        this.clickEffect,
+        this.clickMode
+      );
+    });
+  }
+
+  private initTsParticles(
+    color: string,
+    particleOpacity: number,
+    particlesNumber: number,
+    shapeType: ShapeType,
+    particleSize: number,
+    linesColor: string,
+    linesWidth: number,
+    lineLinked: boolean,
+    linesOpacity: number,
+    linesDistance: number,
+    moveSpeed: number,
+    hoverEffect: boolean,
+    hoverMode: HoverMode,
+    clickEffect: boolean,
+    clickMode: ClickMode
+  ): void {
+    Loader.load(this.id, {
+      fps_limit: 60,
+      interactivity: {
+        detect_on: "canvas",
+        events: {
+          onclick: {
+            enable: clickEffect,
+            mode: clickMode
+          },
+          onhover: {
+            enable: hoverEffect,
+            mode: hoverMode,
+            parallax: {
+              enable: false,
+              force: 2,
+              smooth: 10
+            }
+          },
+          resize: true
+        },
+        modes: {
+          bubble: {
+            distance: 200,
+            duration: 0.4,
+            opacity: 1,
+            size: 80
+          },
+          grab: {
+            distance: 100,
+            line_linked: {
+              opacity: 1
+            }
+          },
+          push: {
+            particles_nb: 4
+          },
+          remove: {
+            particles_nb: 2
+          },
+          repulse: {
+            distance: 200,
+            duration: 0.4
+          }
+        }
+      },
+      particles: {
+        color: {
+          value: color
+        },
+        line_linked: {
+          enable: lineLinked,
+          distance: linesDistance,
+          color: linesColor,
+          opacity: linesOpacity,
+          width: linesWidth
+        },
+        move: {
+          attract: {
+            enable: false,
+            rotateX: 3000,
+            rotateY: 3000
+          },
+          bounce: false,
+          direction: "none",
+          enable: true,
+          out_mode: "out",
+          random: false,
+          speed: moveSpeed,
+          straight: false
+        },
+        number: {
+          density: {
+            enable: true,
+            value_area: 800
+          },
+          value: particlesNumber
+        },
+        opacity: {
+          anim: {
+            enable: false,
+            opacity_min: 0.1,
+            speed: 1,
+            sync: false
+          },
+          random: false,
+          value: particleOpacity
+        },
+        shape: {
+          character: {
+            fill: false,
+            font: "Verdana",
+            style: "",
+            value: "*",
+            weight: "400"
+          },
+          image: {
+            height: 100,
+            replace_color: true,
+            src: "",
+            width: 100
+          },
+          polygon: {
+            nb_sides: 5
+          },
+          stroke: {
+            color: "#ff0000",
+            width: 0
+          },
+          type: shapeType
+        },
+        size: {
+          anim: {
+            enable: false,
+            size_min: 0.1,
+            speed: 40,
+            sync: false
+          },
+          random: true,
+          value: particleSize
+        }
+      },
+      retina_detect: false
+    });
+  }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
