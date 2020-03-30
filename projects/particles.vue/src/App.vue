63e0271d6fa3593acf743e32e06ac31e1b4ebef9
Matteo Bruni <ar3s@icloud.com>
2020-03-30 02:37:09 +0200

    simpler startup

---
+++ projects/particles.vue/src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <img alt="Vue logo" src="./assets/logo.png" />
+    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App" />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+import Particles from "./components/Particles.vue";
+
+@Component({
+  components: {
+    Particles
+  }
+})
+export default class App extends Vue {}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
