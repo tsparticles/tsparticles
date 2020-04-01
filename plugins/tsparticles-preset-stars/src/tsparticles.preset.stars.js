b10e6049e9501c26ac5e96b6014ff433cbbaf960
Matteo Bruni <ar3s@icloud.com>
2020-04-01 20:40:12 +0200

    init

---
+++ plugins/tsparticles-preset-stars/src/tsparticles.preset.stars.js
@@ -0,0 +1,46 @@
+if (!window) {
+    throw new Error('window not found');
+}
+
+if (!tsParticles) {
+    throw new Error('tsParticles not found');
+}
+
+window.addEventListener('load', function () {
+    tsParticles.addPreset('stars', {
+        particles: {
+            color: {
+                value: "#ffffff",
+            },
+            lineLinked: {
+                enable: false,
+            },
+            move: {
+                bounce: false,
+                direction: MoveDirection.none,
+                enable: true,
+                outMode: OutMode.out,
+                random: true,
+                speed: 0.3,
+                straight: false,
+            },
+            opacity: {
+                anim: {
+                    enable: true,
+                    minimumValue: 0,
+                    speed: 1,
+                    sync: false,
+                },
+                random: true,
+                value: 1,
+            },
+            shape: {
+                type: ShapeType.circle,
+            },
+            size: {
+                random: true,
+                value: 3,
+            },
+        },
+    });
+});
