35b9110d3601a8b0c0c7f40dcd85ec5c00d51857
Matteo Bruni <ar3s@icloud.com>
2020-04-02 01:02:31 +0200

    fix

--- plugins/tsparticles-preset-60fps/src/tsparticles.preset.60fps.js
+++ plugins/tsparticles-preset-60fps/src/tsparticles.preset.60fps.js
@@ -1,13 +1,7 @@
-if (!window) {
-    throw new Error('window not found');
-}
-
 if (!tsParticles) {
     throw new Error('tsParticles not found');
 }
 
-window.addEventListener('load', function () {
-    tsParticles.addPreset('60fps', {
-        fpsLimit: 60,
-    });
+tsParticles.addPreset('60fps', {
+    fpsLimit: 60,
 });
