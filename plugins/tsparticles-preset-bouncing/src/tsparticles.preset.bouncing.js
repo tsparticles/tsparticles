198ddb2095519c77349f8f3f20737af2d26c6841
Matteo Bruni <ar3s@icloud.com>
2020-04-01 20:36:39 +0200

    init

---
+++ plugins/tsparticles-preset-bouncing/src/tsparticles.preset.bouncing.js
@@ -0,0 +1,17 @@
+if (!window) {
+    throw new Error('window not found');
+}
+
+if (!tsParticles) {
+    throw new Error('tsParticles not found');
+}
+
+window.addEventListener('load', function () {
+    tsParticles.addPreset('bouncing', {
+        particles: {
+            move: {
+                outMode: OutMode.bounce,
+            },
+        },
+    });
+});
