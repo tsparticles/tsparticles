40659417176ebb6007f5c3ef9dd2b697797357b2
Matteo Bruni <ar3s@icloud.com>
2020-04-02 01:01:56 +0200

    fix

--- plugins/tsparticles-shape-spiral/src/tsparticles.shape.spiral.js
+++ plugins/tsparticles-shape-spiral/src/tsparticles.shape.spiral.js
@@ -1,23 +1,14 @@
-if (!window) {
-    throw new Error('window not found');
-}
-
 if (!tsParticles) {
     throw new Error('tsParticles not found');
 }
 
-/**
- * usage:
- */
-window.addEventListener('load', function () {
-    tsParticles.addShape('spiral', function (context, particle, radius) {
-        var shapeData = particle.shapeData, realWidth = (radius - shapeData.innerRadius) / shapeData.lineSpacing;
+tsParticles.addShape('spiral', function (context, particle, radius) {
+    var shapeData = particle.shapeData, realWidth = (radius - shapeData.innerRadius) / shapeData.lineSpacing;
 
-        for (var i = 0; i < realWidth * 10; i++) {
-            var angle = 0.1 * i, positionFactor = shapeData.innerRadius + shapeData.lineSpacing * angle,
-                x = positionFactor * Math.cos(angle), y = positionFactor * Math.sin(angle);
+    for (var i = 0; i < realWidth * 10; i++) {
+        var angle = 0.1 * i, positionFactor = shapeData.innerRadius + shapeData.lineSpacing * angle,
+            x = positionFactor * Math.cos(angle), y = positionFactor * Math.sin(angle);
 
-            context.lineTo(x, y);
-        }
-    });
+        context.lineTo(x, y);
+    }
 });
