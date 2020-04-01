ae3ba12e939a4ed6e78a2fd932b43ac6c7aa489c
Matteo Bruni <ar3s@icloud.com>
2020-04-02 01:18:08 +0200

    fix

--- plugins/tsparticles-preset-snow/src/tsparticles.preset.snow.js
+++ plugins/tsparticles-preset-snow/src/tsparticles.preset.snow.js
@@ -1,37 +1,31 @@
-if (!window) {
-    throw new Error('window not found');
-}
-
 if (!tsParticles) {
     throw new Error('tsParticles not found');
 }
 
-window.addEventListener('load', function () {
-    tsParticles.addPreset('snow', {
-        particles: {
-            color: {
-                value: "#fff",
-            },
-            move: {
-                bounce: false,
-                direction: 'bottom',
-                enable: true,
-                outMode: 'out',
-                random: false,
-                speed: 2,
-                straight: false,
-            },
-            opacity: {
-                random: true,
-                value: 0.5,
-            },
-            shape: {
-                type: 'circle',
-            },
-            size: {
-                random: true,
-                value: 10,
-            },
+tsParticles.addPreset('snow', {
+    particles: {
+        color: {
+            value: "#fff",
         },
-    });
+        move: {
+            bounce: false,
+            direction: 'bottom',
+            enable: true,
+            outMode: 'out',
+            random: false,
+            speed: 2,
+            straight: false,
+        },
+        opacity: {
+            random: true,
+            value: 0.5,
+        },
+        shape: {
+            type: 'circle',
+        },
+        size: {
+            random: true,
+            value: 10,
+        },
+    },
 });
