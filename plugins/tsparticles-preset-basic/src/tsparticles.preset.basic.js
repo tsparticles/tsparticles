a46305987a2a681c75db85721709c22c8b8eb54f
Matteo Bruni <ar3s@icloud.com>
2020-04-02 01:03:52 +0200

    fix

--- plugins/tsparticles-preset-basic/src/tsparticles.preset.basic.js
+++ plugins/tsparticles-preset-basic/src/tsparticles.preset.basic.js
@@ -1,30 +1,24 @@
-if (!window) {
-    throw new Error('window not found');
-}
-
 if (!tsParticles) {
     throw new Error('tsParticles not found');
 }
 
-window.addEventListener('load', function () {
-    tsParticles.addPreset('basic', {
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            lineLinked: {
-                color: "#ffffff",
-                distance: 150,
-                enable: true,
-                opacity: 0.4,
-                width: 1,
-            },
-            move: {
-                direction: 'none',
-                enable: true,
-                outMode: 'out',
-                speed: 2,
-            },
+tsParticles.addPreset('basic', {
+    particles: {
+        color: {
+            value: "#ffffff",
         },
-    });
+        lineLinked: {
+            color: "#ffffff",
+            distance: 150,
+            enable: true,
+            opacity: 0.4,
+            width: 1,
+        },
+        move: {
+            direction: 'none',
+            enable: true,
+            outMode: 'out',
+            speed: 2,
+        },
+    },
 });
