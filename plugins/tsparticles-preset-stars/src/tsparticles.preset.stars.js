ba619ca4370cb2af1f2f11a3aa8302d0608e9235
Matteo Bruni <ar3s@icloud.com>
2020-04-02 01:18:56 +0200

    fix

--- plugins/tsparticles-preset-stars/src/tsparticles.preset.stars.js
+++ plugins/tsparticles-preset-stars/src/tsparticles.preset.stars.js
@@ -1,46 +1,40 @@
-if (!window) {
-    throw new Error('window not found');
-}
-
 if (!tsParticles) {
     throw new Error('tsParticles not found');
 }
 
-window.addEventListener('load', function () {
-    tsParticles.addPreset('stars', {
-        particles: {
-            color: {
-                value: "#ffffff",
-            },
-            lineLinked: {
-                enable: false,
-            },
-            move: {
-                bounce: false,
-                direction: 'none',
-                enable: true,
-                outMode: 'out',
-                random: true,
-                speed: 0.3,
-                straight: false,
-            },
-            opacity: {
-                anim: {
-                    enable: true,
-                    minimumValue: 0,
-                    speed: 1,
-                    sync: false,
-                },
-                random: true,
-                value: 1,
-            },
-            shape: {
-                type: 'circle',
-            },
-            size: {
-                random: true,
-                value: 3,
-            },
+tsParticles.addPreset('stars', {
+    particles: {
+        color: {
+            value: "#ffffff",
         },
-    });
+        lineLinked: {
+            enable: false,
+        },
+        move: {
+            bounce: false,
+            direction: 'none',
+            enable: true,
+            outMode: 'out',
+            random: true,
+            speed: 0.3,
+            straight: false,
+        },
+        opacity: {
+            anim: {
+                enable: true,
+                minimumValue: 0,
+                speed: 1,
+                sync: false,
+            },
+            random: true,
+            value: 1,
+        },
+        shape: {
+            type: 'circle',
+        },
+        size: {
+            random: true,
+            value: 3,
+        },
+    },
 });
