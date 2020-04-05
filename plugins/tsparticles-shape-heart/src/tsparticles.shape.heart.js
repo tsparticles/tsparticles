e18e3a7eae46be743df6e11cc1aba1df00b71e2c
Matteo Bruni <ar3s@icloud.com>
2020-04-05 13:43:04 +0200

    fixed size

--- plugins/tsparticles-shape-heart/src/tsparticles.shape.heart.js
+++ plugins/tsparticles-shape-heart/src/tsparticles.shape.heart.js
@@ -3,16 +3,16 @@ if (!tsParticles) {
 }
 
 tsParticles.addShape('heart', function (context, particle, radius, opacity) {
-    var x = -radius / 2;
-    var y = -radius / 2;
+    var x = -radius;
+    var y = -radius;
 
-    context.moveTo(x, y + radius / 4);
-    context.quadraticCurveTo(x, y, x + radius / 4, y);
-    context.quadraticCurveTo(x + radius / 2, y, x + radius / 2, y + radius / 4);
-    context.quadraticCurveTo(x + radius / 2, y, x + radius * 3 / 4, y);
-    context.quadraticCurveTo(x + radius, y, x + radius, y + radius / 4);
-    context.quadraticCurveTo(x + radius, y + radius / 2, x + radius * 3 / 4, y + radius * 3 / 4);
-    context.lineTo(x + radius / 2, y + radius);
-    context.lineTo(x + radius / 4, y + radius * 3 / 4);
-    context.quadraticCurveTo(x, y + radius / 2, x, y + radius / 4);
+    context.moveTo(x, y + radius / 2);
+    context.quadraticCurveTo(x, y, x + radius / 2, y);
+    context.quadraticCurveTo(x + radius, y, x + radius, y + radius / 2);
+    context.quadraticCurveTo(x + radius, y, x + radius * 3 / 2, y);
+    context.quadraticCurveTo(x + radius * 2, y, x + radius * 2, y + radius / 2);
+    context.quadraticCurveTo(x + radius * 2, y + radius, x + radius * 3 / 2, y + radius * 3 / 2);
+    context.lineTo(x + radius, y + radius * 2);
+    context.lineTo(x + radius / 2, y + radius * 3 / 2);
+    context.quadraticCurveTo(x, y + radius, x, y + radius / 2);
 });
