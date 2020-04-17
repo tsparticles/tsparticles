70360d363d2f7332839c5633b39223b8eb8982b9
Jonathan Adams <jd.adams16@gmail.com>
2020-04-16 21:17:20 -0500

    Add tests for getDistanceBetweenCoordinates.

--- tests/Utils/Utils.spec.ts
+++ tests/Utils/Utils.spec.ts
@@ -154,4 +154,43 @@ describe('Utils', () => {
 
     });
 
+    describe('getDistanceBetweenCoordinates', () => {
+
+        const point = {x: 1, y: 1};
+        const precision = 1e-10;
+
+        it('should return 0 whenever points are identical', () => {
+            expect(Utils.getDistanceBetweenCoordinates(point, point)).to.be.closeTo(0, precision);
+        });
+
+        it('should calculate correct distance when both points are in first quadrant', () => {
+            const pointA = point;
+            const pointB = {x: 2, y: 2};
+
+            expect(Utils.getDistanceBetweenCoordinates(pointA, pointB)).to.be.closeTo(Math.SQRT2, precision);
+        });
+
+        it('should calculate correct distance when one point is in first quadrant and one is in second quadrant', () => {
+            const pointA = point;
+            const pointB = {x: -1, y: 1};
+
+            expect(Utils.getDistanceBetweenCoordinates(pointA, pointB)).to.be.closeTo(2, precision);
+        });
+
+        it('should calculate correct distance when one point is in first quadrant and one is in third quadrant', () => {
+            const pointA = point;
+            const pointB = {x: -1, y: -1};
+
+            expect(Utils.getDistanceBetweenCoordinates(pointA, pointB)).to.be.closeTo(2*Math.SQRT2, precision);
+        });
+
+        it('should return the same distance regardless of the order of the points', () => {
+            const pointA = point;
+            const pointB = {x: -1, y: -1};
+
+            expect(Utils.getDistanceBetweenCoordinates(pointA, pointB)).to.equal(Utils.getDistanceBetweenCoordinates(pointB, pointA));
+        });
+
+    });
+
 });
