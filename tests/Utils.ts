f3e451e3ad5416eb8bc9397e1e2a9d3c0aa1d574
Jonathan Adams <jd.adams16@gmail.com>
2020-04-17 10:37:41 -0500

    Move file to follow existing structure.

--- tests/Utils.ts
+++ tests/Utils.ts
@@ -1,7 +1,7 @@
 import { expect } from 'chai';
 import 'mocha';
 
-import {Utils} from '../../src/Classes/Utils/Utils';
+import {Utils} from '../src/Classes/Utils/Utils';
  
 describe('Utils', () => {
 
