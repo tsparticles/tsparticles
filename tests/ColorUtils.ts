7a57657c70542bb91ffef759556f1fd9ac8006cf
Matteo Bruni <ar3s@icloud.com>
2020-04-18 00:50:35 +0200

    added other color tests

--- tests/ColorUtils.ts
+++ tests/ColorUtils.ts
@@ -19,6 +19,16 @@ describe('ColorUtils', () => {
 			expect(ColorUtils.colorToRgb(color)).include(red).and.not.be.undefined.and.not.be.null;
 		});
 
+		it('string value', function () {
+			const color: IColor = {
+				value: [ "#ff0000", "#00ff00", "#0000ff" ],
+			};
+
+			expect(ColorUtils.colorToRgb(color)).to.satisfy((rgb: IRgb) => {
+				return rgb.r === 255 || rgb.g === 255 || rgb.b === 255;
+			}).and.not.be.undefined.and.not.be.null;
+		});
+
 		it('IValueColor w/ rgb value', function () {
 			const color: IColor = {
 				value: {
@@ -45,6 +55,30 @@ describe('ColorUtils', () => {
 			};
 
 			expect(ColorUtils.colorToRgb(color)).include(red).and.not.be.undefined.and.not.be.null;
+		});
+
+		it('rgb value', function () {
+			const color: IColor = {
+				value: {
+					b: 0,
+					g: 0,
+					r: 255,
+				},
+			};
+
+			expect(ColorUtils.colorToRgb(color)).include(red).and.not.be.undefined.and.not.be.null;
+		});
+
+		it('hsl value', function () {
+			const color: IColor = {
+				value: {
+					h: 0,
+					l: 50,
+					s: 100,
+				},
+			};
+
+			expect(ColorUtils.colorToRgb(color)).to.include(red).and.not.be.undefined.and.not.be.null;
 		});
 	});
 });
