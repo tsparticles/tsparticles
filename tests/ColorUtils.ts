29f187bff3d053808ce64d60b2409b67b351dd10
Matteo Bruni <ar3s@icloud.com>
2020-04-18 12:43:03 +0200

    more ColorUtils tests added, all methods are covered

--- tests/ColorUtils.ts
+++ tests/ColorUtils.ts
@@ -2,15 +2,17 @@ import { ColorUtils } from '../src/Classes/Utils/ColorUtils';
 import { IColor } from "../src/Interfaces/IColor";
 import { expect } from "chai";
 import { IRgb } from "../src/Interfaces/IRgb";
+import { IHsl } from "../src/Interfaces/IHsl";
+import { IHsla } from "../src/Interfaces/IHsla";
 
 describe('ColorUtils', () => {
-	describe('colorToRgb', () => {
-		const red: IRgb = {
-			b: 0,
-			g: 0,
-			r: 255,
-		};
+	const red: IRgb = {
+		b: 0,
+		g: 0,
+		r: 255,
+	};
 
+	describe('colorToRgb', () => {
 		it('string value', () => {
 			const color: IColor = {
 				value: "#ff0000",
@@ -111,12 +113,6 @@ describe('ColorUtils', () => {
 	});
 
 	describe('stringToRgb', () => {
-		const red: IRgb = {
-			b: 0,
-			g: 0,
-			r: 255,
-		};
-
 		it('from hex string to rgb value', () => {
 			const color = "#ff0000";
 
@@ -130,17 +126,50 @@ describe('ColorUtils', () => {
 		});
 	});
 
-	/*
 	describe('hslToRgb', () => {
+		it('hsl value', () => {
+			const color: IHsl = {
+				h: 0,
+				l: 50,
+				s: 100,
+			};
+
+			expect(ColorUtils.hslToRgb(color)).to.include(red).and.not.be.undefined.and.not.be.null;
+		});
 	});
 
 	describe('hslaToRgba', () => {
+		it('hsl value', () => {
+			const color: IHsla = {
+				a: 1,
+				h: 0,
+				l: 50,
+				s: 100,
+			};
+
+			expect(ColorUtils.hslaToRgba(color)).to.include(red).and.include({ a: 1 }).and.not.be.undefined.and.not.be.null;
+		});
 	});
 
-	describe('getRandomRgbColor', () => {
+	describe('getRandomRgbColor', (min?: number) => {
+		const checkRange = (n: number) => n >= (min ?? 0) && n <= 255;
+
+		const checkColor = (rgb: IRgb, min?: number) => checkRange(rgb.r) && checkRange(rgb.g) && checkRange(rgb.b);
+
+		it('totally random color', () => {
+			expect(ColorUtils.getRandomRgbColor()).to.satisfy((rgb: IRgb) => checkColor(rgb)).and.not.be.undefined.and.not.be.null;
+		});
+
+		it('random color with min seed', () => {
+			const min = 200;
+
+			expect(ColorUtils.getRandomRgbColor(min)).to.satisfy((rgb: IRgb) => checkColor(rgb, min)).and.not.be.undefined.and.not.be.null;
+		});
 	});
 
 	describe('getStyleFromColor', () => {
+		it('IRgb to rgba string', () => {
+			expect(ColorUtils.getStyleFromColor(red)).to.equal("rgba(255, 0, 0, 1)");
+		});
 	});
-	*/
 });
