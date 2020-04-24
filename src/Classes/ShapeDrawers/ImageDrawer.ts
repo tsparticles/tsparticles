063b6a53b8954806f308e3f9457a011ff41a66f2
Matteo Bruni <ar3s@icloud.com>
2020-04-24 15:02:18 +0200

    fixed little issue on ImageDrawer.ts and added a svg data uri in dataImages.json

--- src/Classes/ShapeDrawers/ImageDrawer.ts
+++ src/Classes/ShapeDrawers/ImageDrawer.ts
@@ -7,7 +7,7 @@ export class ImageDrawer implements IShapeDrawer {
             return;
         }
 
-        const imgObj = particle.image?.data.obj;
+        const imgObj = particle.image?.data?.obj;
 
         if (!imgObj) {
             return;
