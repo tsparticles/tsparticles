9ea49709d34dff1c87309f800ba278be311d626a
Simone Bembi <simone.bembi@gmail.com>
2020-04-28 18:15:51 +0200

    Fixes #120

--- components/react-particles-js/index.js
+++ components/react-particles-js/index.js
@@ -1,5 +1,5 @@
 const ReactParticles = require('./cjs/particles');
-for (const key in ReactParticles) {
+for (let key in ReactParticles) {
     ReactParticles.default[key] = ReactParticles[key];
 }
 module.exports = ReactParticles.default;
