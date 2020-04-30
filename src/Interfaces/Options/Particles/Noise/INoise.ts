3e902430171639c0642e640eac585f27a959ffcb
Matteo Bruni <ar3s@icloud.com>
2020-04-30 09:55:48 +0200

    noise improved, every value is now configurable

--- src/Interfaces/Options/Particles/Noise/INoise.ts
+++ src/Interfaces/Options/Particles/Noise/INoise.ts
@@ -1,7 +1,9 @@
-import { INoiseFactor } from "./INoiseFactor";
-import { IOptionLoader } from "../../IOptionLoader";
+import type { INoiseFactor } from "./INoiseFactor";
+import type { IOptionLoader } from "../../IOptionLoader";
+import type { INoiseDelay } from "./INoiseDelay";
 
 export interface INoise extends IOptionLoader<INoise> {
+    delay: INoiseDelay;
     enable: boolean;
     factor: INoiseFactor;
 }
--- src/Interfaces/Options/Particles/Noise/INoiseValue.ts
+++ src/Interfaces/Options/Particles/Noise/INoiseValue.ts
@@ -1,4 +1,4 @@
-import { IOptionLoader } from "../../IOptionLoader";
+import type { IOptionLoader } from "../../IOptionLoader";
 
 export interface INoiseValue extends IOptionLoader<INoiseValue> {
     offset: number;
