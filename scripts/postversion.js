2e1fb2a95d384f5a8fc21426734aa2f4ee2ca4e3
Matteo Bruni <ar3s@icloud.com>
2020-05-07 10:02:44 +0200

    postversion script

---
+++ scripts/postversion.js
@@ -0,0 +1,21 @@
+const fs = require('fs');
+const packageInfo = require('../package.json');
+const filePath = './tsParticles.nuspec';
+
+fs.readFile(filePath, function (error, data) {
+    if (error) {
+        throw error;
+    }
+
+    const text = data.toString();
+
+    console.log(text);
+
+    const newValue = text.replace(/<version>(.*)<\/version>/gm, `<version>${packageInfo.version}</version>`);
+
+    console.log(newValue);
+
+    fs.writeFile(filePath, newValue, 'utf-8', function () {
+        console.log('nuspec updated successfully');
+    });
+});
