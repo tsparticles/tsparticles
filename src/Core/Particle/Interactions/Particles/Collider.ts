16472c929c61d6d47051079cc068b95c18ef0883
Matteo Bruni <ar3s@icloud.com>
2020-06-05 18:37:22 +0200

    splitted resolvecollision method

--- src/Core/Particle/Interactions/Particles/Collider.ts
+++ src/Core/Particle/Interactions/Particles/Collider.ts
@@ -39,91 +39,18 @@ export class Collider {
     }
 
     private static resolveCollision(p1: Particle, p2: Particle, container: Container): void {
-        const pos1 = p1.getPosition();
-        const pos2 = p2.getPosition();
-        const fps = container.options.fpsLimit / 1000;
-
         switch (p1.particlesOptions.collisions.mode) {
             case CollisionMode.absorb: {
-                if (p1.size.value === undefined && p2.size.value !== undefined) {
-                    p1.destroy();
-                } else if (p1.size.value !== undefined && p2.size.value === undefined) {
-                    p2.destroy();
-                } else if (p1.size.value !== undefined && p2.size.value !== undefined) {
-                    if (p1.size.value >= p2.size.value) {
-                        const factor = Utils.clamp(p1.size.value / p2.size.value, 0, p2.size.value) * fps;
-
-                        p1.size.value += factor;
-                        p2.size.value -= factor;
-
-                        if (p2.size.value <= container.retina.pixelRatio) {
-                            p2.size.value = 0;
-                            p2.destroy();
-                        }
-                    } else {
-                        const factor = Utils.clamp(p2.size.value / p1.size.value, 0, p1.size.value) * fps;
-
-                        p1.size.value -= factor;
-                        p2.size.value += factor;
-
-                        if (p1.size.value <= container.retina.pixelRatio) {
-                            p1.size.value = 0;
-                            p1.destroy();
-                        }
-                    }
-                }
+                this.absorb(p1, p2, container);
                 break;
             }
             case CollisionMode.bounce: {
-                const xVelocityDiff = p1.velocity.horizontal - p2.velocity.horizontal;
-                const yVelocityDiff = p1.velocity.vertical - p2.velocity.vertical;
-
-                const xDist = pos2.x - pos1.x;
-                const yDist = pos2.y - pos1.y;
-
-                // Prevent accidental overlap of particles
-                if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
-                    // Grab angle between the two colliding particles
-                    const angle = -Math.atan2(pos2.y - pos1.y, pos2.x - pos1.x);
-
-                    // Store mass in var for better readability in collision equation
-                    const m1 = p1.size.value;
-                    const m2 = p2.size.value;
-
-                    // Velocity before equation
-                    const u1 = this.rotate(p1.velocity, angle);
-                    const u2 = this.rotate(p2.velocity, angle);
-
-                    // Velocity after 1d collision equation
-                    const v1 = this.collisionVelocity(u1, u2, m1, m2);
-                    const v2 = this.collisionVelocity(u2, u1, m1, m2);
-
-                    // Final velocity after rotating axis back to original location
-                    const vFinal1 = this.rotate(v1, -angle);
-                    const vFinal2 = this.rotate(v2, -angle);
-
-                    // Swap particle velocities for realistic bounce effect
-                    p1.velocity.horizontal = vFinal1.horizontal;
-                    p1.velocity.vertical = vFinal1.vertical;
-
-                    p2.velocity.horizontal = vFinal2.horizontal;
-                    p2.velocity.vertical = vFinal2.vertical;
-                }
+                this.bounce(p1, p2);
 
                 break;
             }
             case CollisionMode.destroy: {
-                if (p1.size.value === undefined && p2.size.value !== undefined) {
-                    p1.destroy();
-                } else if (p1.size.value !== undefined && p2.size.value === undefined) {
-                    p2.destroy();
-                } else if (p1.size.value !== undefined && p2.size.value !== undefined) {
-                    if (p1.size.value >= p2.size.value) {
-                        p2.destroy();
-                    } else {
-                        p1.destroy();
-                    }
-                }
+                this.destroy(p1, p2);
 
                 break;
             }
@@ -142,5 +69,91 @@ export class Collider {
             horizontal: (v1.horizontal * (m1 - m2)) / (m1 + m2) + (v2.horizontal * 2 * m2) / (m1 + m2),
             vertical: v1.vertical,
         };
+    }
+
+    private static absorb(p1: Particle, p2: Particle, container: Container): void {
+        const fps = container.options.fpsLimit / 1000;
+
+        if (p1.size.value === undefined && p2.size.value !== undefined) {
+            p1.destroy();
+        } else if (p1.size.value !== undefined && p2.size.value === undefined) {
+            p2.destroy();
+        } else if (p1.size.value !== undefined && p2.size.value !== undefined) {
+            if (p1.size.value >= p2.size.value) {
+                const factor = Utils.clamp(p1.size.value / p2.size.value, 0, p2.size.value) * fps;
+
+                p1.size.value += factor;
+                p2.size.value -= factor;
+
+                if (p2.size.value <= container.retina.pixelRatio) {
+                    p2.size.value = 0;
+                    p2.destroy();
+                }
+            } else {
+                const factor = Utils.clamp(p2.size.value / p1.size.value, 0, p1.size.value) * fps;
+
+                p1.size.value -= factor;
+                p2.size.value += factor;
+
+                if (p1.size.value <= container.retina.pixelRatio) {
+                    p1.size.value = 0;
+                    p1.destroy();
+                }
+            }
+        }
+    }
+
+    private static bounce(p1: Particle, p2: Particle): void {
+        const pos1 = p1.getPosition();
+        const pos2 = p2.getPosition();
+
+        const xVelocityDiff = p1.velocity.horizontal - p2.velocity.horizontal;
+        const yVelocityDiff = p1.velocity.vertical - p2.velocity.vertical;
+
+        const xDist = pos2.x - pos1.x;
+        const yDist = pos2.y - pos1.y;
+
+        // Prevent accidental overlap of particles
+        if (xVelocityDiff * xDist + yVelocityDiff * yDist >= 0) {
+            // Grab angle between the two colliding particles
+            const angle = -Math.atan2(pos2.y - pos1.y, pos2.x - pos1.x);
+
+            // Store mass in var for better readability in collision equation
+            const m1 = p1.size.value;
+            const m2 = p2.size.value;
+
+            // Velocity before equation
+            const u1 = this.rotate(p1.velocity, angle);
+            const u2 = this.rotate(p2.velocity, angle);
+
+            // Velocity after 1d collision equation
+            const v1 = this.collisionVelocity(u1, u2, m1, m2);
+            const v2 = this.collisionVelocity(u2, u1, m1, m2);
+
+            // Final velocity after rotating axis back to original location
+            const vFinal1 = this.rotate(v1, -angle);
+            const vFinal2 = this.rotate(v2, -angle);
+
+            // Swap particle velocities for realistic bounce effect
+            p1.velocity.horizontal = vFinal1.horizontal;
+            p1.velocity.vertical = vFinal1.vertical;
+
+            p2.velocity.horizontal = vFinal2.horizontal;
+            p2.velocity.vertical = vFinal2.vertical;
+        }
+    }
+
+    private static destroy(p1: Particle, p2: Particle): void {
+        if (p1.size.value === undefined && p2.size.value !== undefined) {
+            p1.destroy();
+        } else if (p1.size.value !== undefined && p2.size.value === undefined) {
+            p2.destroy();
+        } else if (p1.size.value !== undefined && p2.size.value !== undefined) {
+            if (p1.size.value >= p2.size.value) {
+                p2.destroy();
+            } else {
+                p1.destroy();
+            }
+        }
     }
 }
