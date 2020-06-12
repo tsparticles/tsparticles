27ac39ec88eccf73cc4b37b33eca659729b9cf4e
Matteo Bruni <ar3s@icloud.com>
2020-06-13 00:23:34 +0200

    jquery wrapper converted in TS

--- wrappers/jquery-particles/demo/app.js
+++ wrappers/jquery-particles/demo/app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const helmet = require('helmet');
 const stylus = require('stylus');
 
 const app = express();
@@ -11,12 +12,12 @@ app.set('views', './demo/views');
 app.set('view engine', 'pug');
 app.use(stylus.middleware('./demo/public'));
 app.use(express.static('./demo/public'));
-app.use("/fontawesome", express.static("./node_modules/@fortawesome/fontawesome-free"));
-app.use("/jsoneditor", express.static("./node_modules/jsoneditor/dist"));
-app.use("/tsparticles", express.static("./node_modules/tsparticles/dist"));
+app.use("/fontawesome", express.static("../../node_modules/@fortawesome/fontawesome-free"));
+app.use("/jsoneditor", express.static("../../node_modules/jsoneditor/dist"));
+app.use("/tsparticles", express.static("../../node_modules/tsparticles/dist"));
 app.use("/jquery-tsparticles", express.static("./dist"));
-app.use("/three", express.static("./node_modules/three/"));
-app.use("/jquery", express.static("./node_modules/jquery/dist/"));
+app.use("/three", express.static("../../node_modules/three/"));
+app.use("/jquery", express.static("../../node_modules/jquery/dist/"));
 
 app.get('/', function (req, res) {
     res.render('index');
