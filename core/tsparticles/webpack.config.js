3bc4d0bbb34b4eea28772e18fe3d3f7fe9e01820
Matteo Bruni <ar3s@icloud.com>
2020-06-14 00:48:36 +0200

    output fix

--- core/tsparticles/webpack.config.js
+++ core/tsparticles/webpack.config.js
@@ -14,71 +14,71 @@ v${version}`;
 const minBanner = `tsParticles v${version} by Matteo Bruni`;
 
 const getConfig = (entry) => {
-  const isSlim = Object.keys(entry).find((t) => t.indexOf("slim") >= 0);
-  const reportFileName = isSlim ? "report.slim" : "report";
+    const isSlim = Object.keys(entry).find((t) => t.indexOf("slim") >= 0);
+    const reportFileName = isSlim ? "report.slim" : "report";
 
-  return {
-    entry: entry,
-    output: {
-      path: path.resolve(__dirname, "dist"),
-      filename: "[name].js",
-      libraryTarget: "window",
-      library: ""
-    },
-    resolve: {
-      extensions: [ ".js", ".json" ]
-    },
-    module: {
-      rules: [
-        {
-          // Include ts, tsx, js, and jsx files.
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "babel-loader"
+    return {
+        entry: entry,
+        output: {
+            path: path.resolve(__dirname, "dist"),
+            filename: "[name].js",
+            libraryTarget: "window",
+            library: ""
+        },
+        resolve: {
+            extensions: [ ".js", ".json" ]
+        },
+        module: {
+            rules: [
+                {
+                    // Include ts, tsx, js, and jsx files.
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    loader: "babel-loader"
+                }
+            ]
+        },
+        plugins: [
+            new webpack.BannerPlugin({
+                banner,
+                exclude: /\.min\.js$/
+            }),
+            new webpack.BannerPlugin({
+                banner: minBanner,
+                include: /\.min\.js$/
+            }),
+            new BundleAnalyzerPlugin({
+                openAnalyzer: false,
+                analyzerMode: "static",
+                exclude: /\.min\.js$/,
+                reportFilename: `${reportFileName}.html`
+            })
+        ],
+        optimization: {
+            minimize: true,
+            minimizer: [
+                new TerserPlugin({
+                    include: /\.min\.js$/,
+                    sourceMap: false,
+                    terserOptions: {
+                        output: {
+                            comments: minBanner
+                        }
+                    },
+                    extractComments: false
+                })
+            ]
         }
-      ]
-    },
-    plugins: [
-      new webpack.BannerPlugin({
-        banner,
-        exclude: /\.min\.js$/
-      }),
-      new webpack.BannerPlugin({
-        banner: minBanner,
-        include: /\.min\.js$/
-      }),
-      new BundleAnalyzerPlugin({
-        openAnalyzer: false,
-        analyzerMode: "static",
-        exclude: /\.min\.js$/,
-        reportFilename: `../demo/public/${reportFileName}.html`
-      })
-    ],
-    optimization: {
-      minimize: true,
-      minimizer: [
-        new TerserPlugin({
-          include: /\.min\.js$/,
-          sourceMap: false,
-          terserOptions: {
-            output: {
-              comments: minBanner
-            }
-          },
-          extractComments: false
-        })
-      ]
-    }
-  };
+    };
 };
 
 module.exports = [
-  getConfig({
-    "tsparticles.slim": "./dist/index.slim.js",
-    "tsparticles.slim.min": "./dist/index.slim.js"
-  }),
-  getConfig({
-    tsparticles: "./dist/index.js",
-    "tsparticles.min": "./dist/index.js"
-  })
+    getConfig({
+        "tsparticles.slim": "./dist/index.slim.js",
+        "tsparticles.slim.min": "./dist/index.slim.js"
+    }),
+    getConfig({
+        tsparticles: "./dist/index.js",
+        "tsparticles.min": "./dist/index.js"
+    })
 ];
