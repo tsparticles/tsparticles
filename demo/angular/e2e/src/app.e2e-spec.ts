736d94f4fa14214685e1b921d0f5beabbdea0334
Matteo Bruni <ar3s@icloud.com>
2020-06-18 09:46:53 +0200

    refactor!: renamed folders

--- demo/angular/e2e/src/app.e2e-spec.ts
+++ demo/angular/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('ng-particles-demo app is running!');
+    expect(page.getTitleText()).toEqual('angular app is running!');
   });
 
   afterEach(async () => {
--- demo/angular/src/app/app.component.ts
+++ demo/angular/src/app/app.component.ts
@@ -6,7 +6,7 @@ import { Component } from '@angular/core';
     styleUrls: [ './app.component.css' ]
 })
 export class AppComponent {
-    title = 'ng-particles-demo';
+    title = 'angular';
     options = {
         fpsLimit: 60,
         particles: {
