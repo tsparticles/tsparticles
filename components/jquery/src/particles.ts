fd89b648ddf1fae2a565d3635152cc58b8bd0ead
Matteo Bruni <ar3s@icloud.com>
2020-07-13 09:52:30 +0200

    fix: jquery plugin improvements

--- components/jquery/src/particles.ts
+++ components/jquery/src/particles.ts
@@ -5,22 +5,24 @@ import { Container } from "tsparticles/dist/Core/Container";
 /**
  * Extend the jQuery result declaration with the example plugin.
  */
+type ParticlesResult = {
+    init: (params: IOptions, callback: (container: Container | undefined) => Promise<void>) => void;
+    ajax: (jsonUrl: string, callback: (container: Container | undefined) => Promise<void>) => void;
+};
+
 declare global {
     interface JQuery {
         /**
          * Extension of the example plugin.
          */
-        particles: () => {
-            init: (params: IOptions, callback: (container: Container | undefined) => Promise<void>) => void;
-            ajax: (jsonUrl: string, callback: (container: Container | undefined) => Promise<void>) => void;
-        };
+        particles: () => ParticlesResult;
     }
 }
 
-$.fn.particles = function () {
+$.fn.particles = function (): ParticlesResult {
     const baseId = "tsparticles";
 
-    const init = (params: IOptions, callback: (container: Container | undefined) => Promise<void>) => {
+    const init = (params: IOptions, callback: (container: Container | undefined) => Promise<void>): void => {
         this.each((index, element) => {
             if (element.id === undefined) {
                 element.id = baseId + Math.floor(Math.random() * 1000);
@@ -30,7 +32,7 @@ $.fn.particles = function () {
         });
     };
 
-    const ajax = (jsonUrl: string, callback: (container: Container | undefined) => Promise<void>) => {
+    const ajax = (jsonUrl: string, callback: (container: Container | undefined) => Promise<void>): void => {
         this.each((index, element) => {
             if (element.id === undefined) {
                 element.id = baseId + Math.floor(Math.random() * 1000);
