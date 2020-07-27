25e90ea8de8196ba8e4b258c319606439fa888c5
Matteo Bruni <ar3s@icloud.com>
2020-07-27 11:38:36 +0200

    chore(svelte): fixed rollup config

--- components/svelte/rollup.config.js
+++ components/svelte/rollup.config.js
@@ -4,19 +4,19 @@ import commonjs from '@rollup/plugin-commonjs';
 import pkg from './package.json';
 
 const name = pkg.name
-	.replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
-	.replace(/^\w/, m => m.toUpperCase())
-	.replace(/-\w/g, m => m[1].toUpperCase());
+    .replace(/^(@\S+\/)?(svelte-)?(\S+)/, '$3')
+    .replace(/^\w/, m => m.toUpperCase())
+    .replace(/-\w/g, m => m[1].toUpperCase());
 
 export default {
-	input: 'src/index.js',
-	output: [
-		{ file: pkg.module, 'format': 'es', file: 'dist/es/svelte-particles.js' },
-		{ file: pkg.main, 'format': 'umd', name, file: 'dist/umd/svelte-particles.js' }
-	],
-	plugins: [
-		svelte(),
-		resolve(),
-		commonjs()
-	]
+    input: 'src/index.js',
+    output: [
+        { file: pkg.module, 'format': 'es' },
+        { file: pkg.main, 'format': 'umd', name }
+    ],
+    plugins: [
+        svelte(),
+        resolve(),
+        commonjs()
+    ]
 };
