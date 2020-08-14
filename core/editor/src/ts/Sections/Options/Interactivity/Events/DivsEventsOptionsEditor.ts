7adc5d89de307a0f8317b8b3f93ddf29e4b27036
Matteo Bruni <ar3s@icloud.com>
2020-08-14 10:59:47 +0200

    chore(editor): added some divs properties

--- core/editor/src/ts/Sections/Options/Interactivity/Events/DivsEventsOptionsEditor.ts
+++ core/editor/src/ts/Sections/Options/Interactivity/Events/DivsEventsOptionsEditor.ts
@@ -3,6 +3,8 @@ import { Container } from "tsparticles/dist/Core/Container";
 import type { SingleOrMultiple } from "tsparticles";
 import type { IDivEvent } from "tsparticles/dist/Options/Interfaces/Interactivity/Events/IDivEvent";
 import { EditorBase } from "../../../../EditorBase";
+import { EditorType } from "object-gui/dist/js";
+import { DivType } from "tsparticles";
 
 export class DivsEventsOptionsEditor extends EditorBase {
     public group!: EditorGroup;
@@ -20,8 +22,52 @@ export class DivsEventsOptionsEditor extends EditorBase {
     }
 
     private addDivs(): void {
-        // const particles = this.particles;
-        // const options = this.options;
-        // const divsGroups = this.group;
+        const options = this.options;
+
+        if (options instanceof Array) {
+            for (let i = 0; i < options.length; i++) {
+                const group = this.group.addGroup(`div_${i + 1}`, `Div_${i + 1}`, true, options[i]);
+
+                this.addDiv(group);
+            }
+        } else {
+            this.addDiv(this.group);
+        }
+    }
+
+    private addDiv(group: EditorGroup) {
+        const particles = this.particles;
+        const options = group.data as IDivEvent;
+
+        group
+            .addProperty("enable", "Enable", EditorType.boolean)
+            .change(async () => {
+                await particles.refresh();
+            })
+            .step(0.01)
+            .min(0)
+            .max(1);
+
+        if (options.selectors instanceof Array) {
+            // TODO: add array support to selectors property
+        } else {
+            group.addProperty("selectors", "Selectors", EditorType.string).change(async () => {
+                await particles.refresh();
+            });
+        }
+
+        group
+            .addProperty("type", "Type", EditorType.select)
+            .change(async () => {
+                await particles.refresh();
+            })
+            .addItems([
+                {
+                    value: DivType.circle,
+                },
+                {
+                    value: DivType.rectangle,
+                },
+            ]);
     }
 }
