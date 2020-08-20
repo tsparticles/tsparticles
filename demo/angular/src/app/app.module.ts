1c1ec4b8b849989cd3a730526cb5e0149d096cae
Matteo Bruni <ar3s@icloud.com>
2020-08-20 17:11:09 +0200

    fix: angular demo improved

--- demo/angular/src/app/app.module.ts
+++ demo/angular/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { NgParticlesModule } from 'ng-particles';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
     declarations: [
-        AppComponent
+        AppComponent,
+        ContainerComponent
     ],
     imports: [
         BrowserModule,
