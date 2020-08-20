16a219bffecd38b7e30d6a008dba0c0d0f0cf6eb
Matteo Bruni <ar3s@icloud.com>
2020-08-20 14:25:35 +0200

    fix: fixed angular demo

--- demo/angular/src/app/app.module.ts
+++ demo/angular/src/app/app.module.ts
@@ -1,9 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { NgParticlesModule } from 'ng-particles/dist/ng-particles';
-// ng-particles import path for working with lerna (production use is just ng-particles)
+import { NgParticlesModule } from 'ng-particles';
 
 @NgModule({
     declarations: [
