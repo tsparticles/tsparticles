ede160bf3b9ce912ac0fb80212908c99217adec0
Matteo Bruni <ar3s@icloud.com>
2020-08-29 03:09:24 +0200

    build: started working on website (issue #752) using React

---
+++ website/src/routes/home/index.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "./components/header";
+
+export default function Index() {
+    return (
+        <div className="App">
+            <Header/>
+        </div>
+    );
+}
