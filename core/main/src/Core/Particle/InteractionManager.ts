212adb99a959865cb5db81691492f5800af0c8ba
Matteo Bruni <ar3s@icloud.com>
2020-09-23 01:22:33 +0200

    build: renamed interaction mouse folder

--- core/main/src/Core/Particle/InteractionManager.ts
+++ core/main/src/Core/Particle/InteractionManager.ts
@@ -1,7 +1,7 @@
-import { Grabber } from "../../Interactions/Mouse/Grabber";
-import { Repulser } from "../../Interactions/Mouse/Repulser";
-import { Bubbler } from "../../Interactions/Mouse/Bubbler";
-import { Connector } from "../../Interactions/Mouse/Connector";
+import { Grabber } from "../../Interactions/External/Grabber";
+import { Repulser } from "../../Interactions/External/Repulser";
+import { Bubbler } from "../../Interactions/External/Bubbler";
+import { Connector } from "../../Interactions/External/Connector";
 import { Container } from "../Container";
 import { Linker } from "../../Interactions/Particles/Linker";
 import { Attractor as ParticlesAttractor } from "../../Interactions/Particles/Attractor";
@@ -9,12 +9,12 @@ import { Collider } from "../../Interactions/Particles/Collider";
 import { Infecter } from "../../Interactions/Particles/Infecter";
 import type { IExternalInteractor } from "../Interfaces/IExternalInteractor";
 import type { IParticlesInteractor } from "../Interfaces/IParticlesInteractor";
-import { TrailMaker } from "../../Interactions/Mouse/TrailMaker";
+import { TrailMaker } from "../../Interactions/External/TrailMaker";
 import type { IDelta } from "../Interfaces/IDelta";
-import { Attractor as MouseAttractor } from "../../Interactions/Mouse/Attractor";
+import { Attractor as MouseAttractor } from "../../Interactions/External/Attractor";
 import { Particle } from "../Particle";
 import { Lighter as ParticlesLighter } from "../../Interactions/Particles/Lighter";
-import { Lighter as MouseLighter } from "../../Interactions/Mouse/Lighter";
+import { Lighter as MouseLighter } from "../../Interactions/External/Lighter";
 
 /**
  * @category Core
