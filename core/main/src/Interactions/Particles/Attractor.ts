d5ac5f7ee36e8b9fdc7b2fb19c30ab74e037790e
Matteo Bruni <ar3s@icloud.com>
2020-10-30 08:52:31 +0100

    build: prettiefied code

--- core/main/src/Interactions/Particles/Attractor.ts
+++ core/main/src/Interactions/Particles/Attractor.ts
@@ -8,8 +8,7 @@ import { NumberUtils } from "../../Utils";
  * @category Interactions
  */
 export class Attractor implements IParticlesInteractor {
-    constructor(private readonly container: Container) {
-    }
+    constructor(private readonly container: Container) {}
 
     public interact(p1: IParticle): void {
         const container = this.container;
