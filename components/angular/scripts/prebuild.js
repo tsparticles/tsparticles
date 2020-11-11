f0ebecdde4bd86d0d0c0f7088935a0d4c822f3dc
Matteo Bruni <ar3s@icloud.com>
2020-11-11 09:28:53 +0100

    build: updated packages

--- components/angular/scripts/prebuild.js
+++ components/angular/scripts/prebuild.js
@@ -12,7 +12,7 @@ fs.readFile(libPackage, function (error, data) {
     const libObj = JSON.parse(text);
 
     libObj.version = mainPackage.version;
-    libObj.peerDependencies.tsparticles = mainPackage.dependencies.tsparticles;
+    libObj.peerDependencies.tsparticles = mainPackage.dependencies.tsparticles.replace("workspace:", "");
 
     fs.writeFile(libPackage, JSON.stringify(libObj, undefined, 2), 'utf-8', function () {
         console.log(`lib package.json updated successfully to version ${mainPackage.version}`);
