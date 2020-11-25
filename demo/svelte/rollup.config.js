2258ed240b476595b72655ce25ccc3e8c193644e
Matteo Bruni <ar3s@icloud.com>
2020-11-25 08:49:13 +0100

    build: updated svelte rollup plugin and fixed build errors

--- demo/svelte/rollup.config.js
+++ demo/svelte/rollup.config.js
@@ -18,13 +18,7 @@ export default {
     },
     plugins: [
         svelte({
-            // enable run-time checks when not in production
-            dev: !production,
-            // we'll extract any component CSS out into
-            // a separate file - better for performance
-            css: css => {
-                css.write('public/dist/bundle.css');
-            },
+            emitCss: false,
 			preprocess: sveltePreprocess(),
         }),
 
