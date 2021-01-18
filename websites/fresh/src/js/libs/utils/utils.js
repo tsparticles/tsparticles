93342f5effc46082315deea833d2fb29d81945ba
cssninja <35071927+cssninjaStudio@users.noreply.github.com>
2021-01-18 09:26:25 +0100

    fixed modals and prevented scroll when modal is open

--- websites/fresh/src/js/libs/utils/utils.js
+++ websites/fresh/src/js/libs/utils/utils.js
@@ -42,19 +42,44 @@ export function initModals() {
   let targets = document.querySelectorAll(".modal-trigger");
   if (typeof targets != "undefined" && targets != null) {
     for (var i = 0, len = targets.length; i < len; i++) {
-      targets[i].addEventListener('click',  function (event) {
-        var modalID = this.getAttribute('data-modal');
-        document.querySelector('#' + modalID).classList.add('is-active');
-      })
+      targets[i].addEventListener("click", function (event) {
+        console.log("click modal");
+        var modalID = this.getAttribute("data-modal");
+        document.querySelector("#" + modalID).classList.add("is-active");
+        const scrollY = document.documentElement.style.getPropertyValue(
+          "--scroll-y"
+        );
+        const body = document.body;
+        body.style.width = "100%";
+        body.style.paddingRight  = "15px";
+        body.style.position = "fixed";
+        
+        body.style.top = `-${scrollY}`;
+      });
     }
   }
 
-  targets = document.querySelectorAll(".modal-close");
+  targets = document.querySelectorAll(".modal-dismiss");
   if (typeof targets != "undefined" && targets != null) {
     for (var i = 0, len = targets.length; i < len; i++) {
-      targets[i].addEventListener('click',  function (event) {
-        this.closest('.modal').classList.remove('is-active');
-      })
+      targets[i].addEventListener("click", function (event) {
+        console.log("click modal close");
+        const body = document.body;
+        const scrollY = body.style.top;
+        body.style.position = "";
+        body.style.paddingRight  = "";
+        body.style.width = "";
+        body.style.top = "";
+        window.scrollTo(0, parseInt(scrollY || "0") * -1);
+        this.closest(".modal").classList.remove("is-active");
+      });
     }
   }
+
+  window.addEventListener("scroll", () => {
+    document.documentElement.style.setProperty(
+      "--scroll-y",
+      `${window.scrollY}px`
+    );
+  });
 }
