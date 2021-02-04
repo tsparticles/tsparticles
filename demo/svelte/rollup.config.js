fa2eea7449d7763ea76c010e595372b524e10b3c
Matteo Bruni <ar3s@icloud.com>
2021-02-04 01:09:46 +0100

    build: working on svelte demo

--- demo/svelte/rollup.config.js
+++ demo/svelte/rollup.config.js
@@ -1,69 +1,77 @@
-import svelte from 'rollup-plugin-svelte';
-import resolve from '@rollup/plugin-node-resolve';
-import commonjs from '@rollup/plugin-commonjs';
-import livereload from 'rollup-plugin-livereload';
-import { terser } from 'rollup-plugin-terser';
-import sveltePreprocess from 'svelte-preprocess';
-import typescript from '@rollup/plugin-typescript';
+import svelte from "rollup-plugin-svelte";
+import resolve from "@rollup/plugin-node-resolve";
+import commonjs from "@rollup/plugin-commonjs";
+import livereload from "rollup-plugin-livereload";
+import { terser } from "rollup-plugin-terser";
+import sveltePreprocess from "svelte-preprocess";
+import typescript from "@rollup/plugin-typescript";
 
 const production = !process.env.ROLLUP_WATCH;
 
 export default {
-    input: 'src/main.ts',
-    output: {
-        sourcemap: true,
-        format: 'iife',
-        name: 'app',
-        file: 'public/dist/bundle.js'
-    },
-    plugins: [
-        svelte({
-            emitCss: false,
-			preprocess: sveltePreprocess(),
-        }),
-
-        // If you have external dependencies installed from
-        // npm, you'll most likely need these plugins. In
-        // some cases you'll need additional configuration -
-        // consult the documentation for details:
-        // https://github.com/rollup/plugins/tree/master/packages/commonjs
-        resolve({
-            browser: true,
-            dedupe: [ 'svelte', "svelte-particles", "tsparticles" ]
-        }),
-        commonjs(),
-		typescript({ sourceMap: !production }),
-
-        // In dev mode, call `npm run start` once
-        // the bundle has been generated
-        !production && serve(),
-
-        // Watch the `public` directory and refresh the
-        // browser on changes when not in production
-        !production && livereload('public'),
-
-        // If we're building for production (npm run build
-        // instead of npm run dev), minify
-        production && terser()
-    ],
-    watch: {
-        clearScreen: false
+  input: "src/main.ts",
+  output: {
+    sourcemap: true,
+    format: "iife",
+    name: "app",
+    file: "public/dist/bundle.js",
+    globals: {
+      "tsparticles": "window",
+      "tsparticles-core": "window"
     }
+  },
+  external: [
+    "tsparticles",
+    "tsparticles-core"
+  ],
+  plugins: [
+    svelte({
+      emitCss: false,
+      preprocess: sveltePreprocess()
+    }),
+
+    // If you have external dependencies installed from
+    // npm, you'll most likely need these plugins. In
+    // some cases you'll need additional configuration -
+    // consult the documentation for details:
+    // https://github.com/rollup/plugins/tree/master/packages/commonjs
+    resolve({
+      browser: true,
+      dedupe: [ "svelte", "svelte-particles", "tsparticles", "tsparticles-core" ]
+    }),
+    commonjs(),
+    typescript({ sourceMap: true }),
+
+    // In dev mode, call `npm run start` once
+    // the bundle has been generated
+    !production && serve(),
+
+    // Watch the `public` directory and refresh the
+    // browser on changes when not in production
+    !production && livereload("public"),
+
+    // If we're building for production (npm run build
+    // instead of npm run dev), minify
+    production && terser()
+  ],
+  watch: {
+    clearScreen: false
+  }
 };
 
 function serve() {
-    let started = false;
+  let started = false;
 
-    return {
-        writeBundle() {
-            if (!started) {
-                started = true;
+  return {
+    writeBundle() {
+      if (!started) {
+        started = true;
 
-                require('child_process').spawn('npm', [ 'run', 'start', '--', '--dev' ], {
-                    stdio: [ 'ignore', 'inherit', 'inherit' ],
-                    shell: true
-                });
-            }
-        }
-    };
+        require("child_process").spawn("npm", [ "run", "start", "--", "--dev" ], {
+          stdio: [ "ignore", "inherit", "inherit" ],
+          shell: true
+        });
+      }
+    }
+  };
 }
