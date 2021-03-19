665fd76babe0bf396830ffbe372e21d2583bc657
Matteo Bruni <ar3s@icloud.com>
2021-03-19 02:54:48 +0100

    build: fixed merge

--- engine/src/Options/Classes/Particles/Move/Path/MovePathDelay.ts
+++ engine/src/Options/Classes/Particles/Move/Path/MovePathDelay.ts
@@ -1,10 +1,6 @@
 import { ValueWithRandom } from "../../../ValueWithRandom";
 
-<<<<<<< HEAD:engine/src/Options/Classes/Particles/Move/Path/MovePathDelay.ts
 export class MovePathDelay extends ValueWithRandom {
-=======
-export class PathDelay extends ValueWithRandom {
->>>>>>> main:engine/src/Options/Classes/Move/Path/PathDelay.ts
     constructor() {
         super();
     }
