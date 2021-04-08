1b373bd976aed80ed106e685fc1fcbe557c2ba08
Matteo Bruni <ar3s@icloud.com>
2021-04-09 01:20:45 +0200

    feat: added a first release of riot.js component

---
+++ components/riot/readme.md
@@ -0,0 +1,26 @@
+# riot-particles
+
+## installation
+
+```
+npm i riot-particles
+```
+
+## Usage
+
+```riot
+<app>
+  <riot-particles/>
+
+  <script>
+    import RiotParticles from 'riot-particles'
+
+    export default {
+      components: {
+        RiotParticles
+      }
+    }
+  </script>
+</app>
+
+```
---
+++ components/riot/test.js
@@ -0,0 +1,17 @@
+import MyComponent from './'
+import { expect } from 'chai'
+import { component } from 'riot'
+
+describe('My Component Unit Test', () => {
+  const mountMyComponent = component(MyComponent)
+
+  it('The component properties are properly rendered', () => {
+    const div = document.createElement('div')
+
+    const component = mountMyComponent(div, {
+      message: 'hello'
+    })
+
+    expect(component.$('p').innerHTML).to.be.equal('hello')
+  })
+})
