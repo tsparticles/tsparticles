2a6adc8ce02a3f4713e4fb66d524a8351513d665
Matteo Bruni <ar3s@icloud.com>
2021-05-22 15:22:45 +0200

    build: prettier

--- presets/fountain/src/options.ts
+++ presets/fountain/src/options.ts
@@ -17,7 +17,7 @@ export const options: ISourceOptions = {
             },
         },
         color: {
-            value: [ "#3998D0", "#2EB6AF", "#A9BD33", "#FEC73B", "#F89930", "#F45623", "#D62E32", "#EB586E", "#9952CF" ],
+            value: ["#3998D0", "#2EB6AF", "#A9BD33", "#FEC73B", "#F89930", "#F45623", "#D62E32", "#EB586E", "#9952CF"],
         },
         number: {
             value: 0,
