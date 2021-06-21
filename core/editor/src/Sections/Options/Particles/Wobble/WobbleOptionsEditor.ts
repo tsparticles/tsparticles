a16e164bf71d063edb557ce481a218fb68fc437d
Matteo Bruni <ar3s@icloud.com>
2021-06-21 15:38:13 +0200

    feat: added new sections to editor

---
+++ core/editor/src/Sections/Options/Particles/Wobble/WobbleOptionsEditor.ts
@@ -0,0 +1,36 @@
+import type { Container } from "tsparticles";
+import { EditorGroup, EditorType } from "object-gui";
+import { EditorBase } from "../../../../EditorBase";
+import type { IWobble } from "tsparticles/dist/Options/Interfaces/Particles/Wobble/IWobble";
+
+export class WobbleOptionsEditor extends EditorBase {
+    group!: EditorGroup;
+    private options!: IWobble;
+
+    constructor(particles: Container) {
+        super(particles);
+    }
+
+    addToGroup(parent: EditorGroup): void {
+        this.group = parent.addGroup("wobble", "Wobble");
+        this.options = this.group.data as IWobble;
+
+        this.addProperties();
+    }
+
+    private addProperties(): void {
+        const particles = this.particles;
+
+        this.group.addProperty("distance", "Distance", EditorType.number).change(async () => {
+            await particles.refresh();
+        });
+
+        this.group.addProperty("enable", "Enable", EditorType.boolean).change(async () => {
+            await particles.refresh();
+        });
+
+        this.group.addProperty("speed", "Speed", EditorType.number).change(async () => {
+            await particles.refresh();
+        });
+    }
+}
