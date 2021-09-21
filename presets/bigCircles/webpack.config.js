3abb0feff6e8fe844b3433aa9239e0b363e0b247
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-09-22 01:12:31 +0200

    build: updated big circles and bubbles presets

--- presets/bigCircles/webpack.config.js
+++ presets/bigCircles/webpack.config.js
@@ -19,8 +19,6 @@ const getExternals = (bundle) => {
     }
 
     return [
-        /*
- */
         {
             "tsparticles-engine/Updaters/Color": {
                 commonjs: "tsparticles-engine/Updaters/Color",
--- presets/bubbles/src/index.ts
+++ presets/bubbles/src/index.ts
@@ -1,6 +1,19 @@
 import type { Main } from "tsparticles-engine";
 import { options } from "./options";
+import { loadCircleShape } from "tsparticles-engine/Shapes/Circle";
+import { loadColorUpdater } from "tsparticles-engine/Updaters/Color";
+import { loadOpacityUpdater } from "tsparticles-engine/Updaters/Opacity";
+import { loadOutModesUpdater } from "tsparticles-engine/Updaters/OutModes";
+import { loadEmittersPlugin } from "tsparticles-engine/Plugins/Emitters/plugin";
+import { loadSizeUpdater } from "tsparticles-engine/Updaters/Size";
 
 export function loadBubblesPreset(tsParticles: Main): void {
+    loadCircleShape(tsParticles);
+    loadColorUpdater(tsParticles);
+    loadSizeUpdater(tsParticles);
+    loadOpacityUpdater(tsParticles);
+    loadOutModesUpdater(tsParticles);
+    loadEmittersPlugin(tsParticles);
+
     tsParticles.addPreset("bubbles", options);
 }
