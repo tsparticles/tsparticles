ef4ba0457b54ff269f62958d7930dc77e7a5bfa1
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-09-22 21:20:42 +0200

    build: added plugins to confetti preset

--- presets/confetti/src/preset.ts
+++ presets/confetti/src/preset.ts
@@ -2,8 +2,33 @@ import type { Main, RecursivePartial } from "tsparticles-engine";
 import { IConfettiOptions } from "./IConfettiOptions";
 import { tsParticles } from "tsparticles-engine";
 import { loadOptions } from "./options";
+import { loadCircleShape } from "tsparticles-engine/Shapes/Circle";
+import { loadColorUpdater } from "tsparticles-engine/Updaters/Color";
+import { loadOpacityUpdater } from "tsparticles-engine/Updaters/Opacity";
+import { loadOutModesUpdater } from "tsparticles-engine/Updaters/OutModes";
+import { loadEmittersPlugin } from "tsparticles-engine/Plugins/Emitters/plugin";
+import { loadSizeUpdater } from "tsparticles-engine/Updaters/Size";
+import { loadWobbleUpdater } from "tsparticles-engine/Updaters/Wobble";
+import { loadRollUpdater } from "tsparticles-engine/Updaters/Roll";
+import { loadAngleUpdater } from "tsparticles-engine/Updaters/Angle";
+import { loadTiltUpdater } from "tsparticles-engine/Updaters/Tilt";
+import { loadSquareShape } from "tsparticles-engine/Shapes/Square";
+import { loadLifeUpdater } from "tsparticles-engine/Updaters/Life";
 
 function loadPreset(main: Main, confettiOptions: RecursivePartial<IConfettiOptions>, override = false): void {
+    loadCircleShape(tsParticles);
+    loadSquareShape(tsParticles);
+    loadColorUpdater(tsParticles);
+    loadSizeUpdater(tsParticles);
+    loadOpacityUpdater(tsParticles);
+    loadOutModesUpdater(tsParticles);
+    loadEmittersPlugin(tsParticles);
+    loadWobbleUpdater(tsParticles);
+    loadRollUpdater(tsParticles);
+    loadAngleUpdater(tsParticles);
+    loadTiltUpdater(tsParticles);
+    loadLifeUpdater(tsParticles);
+
     main.addPreset("confetti", loadOptions(confettiOptions), override);
 }
 
--- presets/confetti/webpack.config.js
+++ presets/confetti/webpack.config.js
@@ -20,6 +20,110 @@ const getExternals = (bundle) => {
 
     return [
         {
+            "tsparticles-engine/Updaters/Color": {
+                commonjs: "tsparticles-engine/Updaters/Color",
+                commonjs2: "tsparticles-engine/Updaters/Color",
+                amd: "tsparticles-engine/Updaters/Color",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Plugins/Emitters/plugin": {
+                commonjs: "tsparticles-engine/Plugins/Emitters/plugin",
+                commonjs2: "tsparticles-engine/Plugins/Emitters/plugin",
+                amd: "tsparticles-engine/Plugins/Emitters/plugin",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Shapes/Circle": {
+                commonjs: "tsparticles-engine/Shapes/Circle",
+                commonjs2: "tsparticles-engine/Shapes/Circle",
+                amd: "tsparticles-engine/Shapes/Circle",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Size": {
+                commonjs: "tsparticles-engine/Updaters/Size",
+                commonjs2: "tsparticles-engine/Updaters/Size",
+                amd: "tsparticles-engine/Updaters/Size",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Opacity": {
+                commonjs: "tsparticles-engine/Updaters/Opacity",
+                commonjs2: "tsparticles-engine/Updaters/Opacity",
+                amd: "tsparticles-engine/Updaters/Opacity",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/OutModes": {
+                commonjs: "tsparticles-engine/Updaters/OutModes",
+                commonjs2: "tsparticles-engine/Updaters/OutModes",
+                amd: "tsparticles-engine/Updaters/OutModes",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Size": {
+                commonjs: "tsparticles-engine/Updaters/Size",
+                commonjs2: "tsparticles-engine/Updaters/Size",
+                amd: "tsparticles-engine/Updaters/Size",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Wobble": {
+                commonjs: "tsparticles-engine/Updaters/Wobble",
+                commonjs2: "tsparticles-engine/Updaters/Wobble",
+                amd: "tsparticles-engine/Updaters/Wobble",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Roll": {
+                commonjs: "tsparticles-engine/Updaters/Roll",
+                commonjs2: "tsparticles-engine/Updaters/Roll",
+                amd: "tsparticles-engine/Updaters/Roll",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Angle": {
+                commonjs: "tsparticles-engine/Updaters/Angle",
+                commonjs2: "tsparticles-engine/Updaters/Angle",
+                amd: "tsparticles-engine/Updaters/Angle",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Tilt": {
+                commonjs: "tsparticles-engine/Updaters/Tilt",
+                commonjs2: "tsparticles-engine/Updaters/Tilt",
+                amd: "tsparticles-engine/Updaters/Tilt",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Shapes/Square": {
+                commonjs: "tsparticles-engine/Shapes/Square",
+                commonjs2: "tsparticles-engine/Shapes/Square",
+                amd: "tsparticles-engine/Shapes/Square",
+                root: "window"
+            }
+        },
+        {
+            "tsparticles-engine/Updaters/Life": {
+                commonjs: "tsparticles-engine/Updaters/Life",
+                commonjs2: "tsparticles-engine/Updaters/Life",
+                amd: "tsparticles-engine/Updaters/Life",
+                root: "window"
+            }
+        },
+        {
             "tsparticles": {
                 commonjs: "tsparticles",
                 commonjs2: "tsparticles",
