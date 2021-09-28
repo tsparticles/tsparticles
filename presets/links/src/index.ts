9812393eafea002d7d68dc5b302c5c56346e3319
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-09-28 23:02:41 +0200

    feat: fixed links presets

--- presets/links/src/index.ts
+++ presets/links/src/index.ts
@@ -1,6 +1,15 @@
 import type { Main } from "tsparticles-engine";
 import { options } from "./options";
+import { loadCircleShape } from "tsparticles-shape-circle";
+import { loadParticlesLinksInteraction } from "tsparticles-interaction-particles-links";
+import { loadOutModesUpdater } from "tsparticles-updater-out-modes";
+import { loadSizeUpdater } from "tsparticles-updater-size";
 
 export function loadLinksPreset(tsParticles: Main): void {
+    loadCircleShape(tsParticles);
+    loadParticlesLinksInteraction(tsParticles);
+    loadOutModesUpdater(tsParticles);
+    loadSizeUpdater(tsParticles);
+
     tsParticles.addPreset("links", options);
 }
