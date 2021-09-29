fdd4d8b13ae7c23a5bafd001d2f0169193804f12
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-09-29 20:23:59 +0200

    feat: fixed links and triangles presets

--- presets/links/src/index.ts
+++ presets/links/src/index.ts
@@ -4,11 +4,15 @@ import { loadCircleShape } from "tsparticles-shape-circle";
 import { loadParticlesLinksInteraction } from "tsparticles-interaction-particles-links";
 import { loadOutModesUpdater } from "tsparticles-updater-out-modes";
 import { loadSizeUpdater } from "tsparticles-updater-size";
+import { loadColorUpdater } from "tsparticles-updater-color";
+import { loadOpacityUpdater } from "tsparticles-updater-opacity";
 
 export function loadLinksPreset(tsParticles: Main): void {
     loadCircleShape(tsParticles);
+    loadColorUpdater(tsParticles);
     loadParticlesLinksInteraction(tsParticles);
     loadOutModesUpdater(tsParticles);
+    loadOpacityUpdater(tsParticles);
     loadSizeUpdater(tsParticles);
 
     tsParticles.addPreset("links", options);
--- presets/triangles/src/index.ts
+++ presets/triangles/src/index.ts
@@ -1,6 +1,19 @@
 import type { Main } from "tsparticles-engine";
 import { options } from "./options";
+import { loadCircleShape } from "tsparticles-shape-circle";
+import { loadParticlesLinksInteraction } from "tsparticles-interaction-particles-links";
+import { loadOutModesUpdater } from "tsparticles-updater-out-modes";
+import { loadSizeUpdater } from "tsparticles-updater-size";
+import { loadColorUpdater } from "tsparticles-updater-color";
+import { loadOpacityUpdater } from "tsparticles-updater-opacity";
 
 export function loadTrianglesPreset(tsParticles: Main): void {
+    loadCircleShape(tsParticles);
+    loadColorUpdater(tsParticles);
+    loadParticlesLinksInteraction(tsParticles);
+    loadOutModesUpdater(tsParticles);
+    loadOpacityUpdater(tsParticles);
+    loadSizeUpdater(tsParticles);
+
     tsParticles.addPreset("triangles", options);
 }
