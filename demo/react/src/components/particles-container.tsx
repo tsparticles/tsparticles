5e1f51522aeb8fe679ef19df1290421ea1f15bdd
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-10-01 01:12:29 +0200

    build: fixed react demo

--- demo/react/src/components/particles-container.tsx
+++ demo/react/src/components/particles-container.tsx
@@ -1,13 +1,20 @@
 import * as React from 'react';
 import Particles from 'react-tsparticles';
-import { IOptions, RecursivePartial } from "tsparticles-engine";
+import type { IOptions, RecursivePartial } from "tsparticles-engine";
+import type { Main } from "tsparticles-engine";
+import { loadFull } from "tsparticles";
 
 interface IProps {
     options: RecursivePartial<IOptions>;
 }
 
 export class ParticlesContainer extends React.PureComponent<IProps> {
+    particlesInit(main: Main): void {
+        loadFull(main);
+    }
+
     render() {
-        return <Particles options={this.props.options} className="frame-layout__particles"/>
+        return <Particles options={ this.props.options } className="frame-layout__particles"
+                          init={ this.particlesInit.bind(this) }/>
     }
 }
