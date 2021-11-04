0449fa08d35951cb8d2fcf0d7c07e8dd7d5717eb
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-11-04 20:15:50 +0100

    build: updated to angular v13, kept previous angular lib and demo

--- demo/angular/src/app/app.module.ts
+++ demo/angular/src/app/app.module.ts
@@ -1,16 +1,20 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { NgParticlesModule } from 'ng-particles';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContainerComponent } from './container/container.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContainerComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgParticlesModule
   ],
   providers: [],
   bootstrap: [AppComponent]
