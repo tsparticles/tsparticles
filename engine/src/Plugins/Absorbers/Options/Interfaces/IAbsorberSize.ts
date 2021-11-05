f2d0493f34bc373846d8a8ae78e36dd56816308e
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-11-05 14:44:23 +0100

    feat: added more absorber size limit options fix: fixed absorbers particle repositioning, resetted velocity to initial velocity

--- engine/src/Plugins/Absorbers/Options/Interfaces/IAbsorberSize.ts
+++ engine/src/Plugins/Absorbers/Options/Interfaces/IAbsorberSize.ts
@@ -1,6 +1,7 @@
-import { IValueWithRandom } from "../../../../Options/Interfaces/IValueWithRandom";
+import type { IValueWithRandom } from "../../../../Options/Interfaces/IValueWithRandom";
+import type { IAbsorberSizeLimit } from "./IAbsorberSizeLimit";
 
 export interface IAbsorberSize extends IValueWithRandom {
-    limit?: number;
+    limit?: number | IAbsorberSizeLimit;
     density: number;
 }
