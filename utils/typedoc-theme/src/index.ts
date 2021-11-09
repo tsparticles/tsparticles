99cd6dd83d955ae468f75c132e92ddeff851dc17
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-11-09 19:01:19 +0100

    build: fixed docs theme

--- utils/typedoc-theme/src/index.ts
+++
@@ -1,6 +0,0 @@
-import { Application } from "typedoc";
-import { CustomTheme } from "./theme";
-
-export function load(app: Application): void {
-    app.renderer.defineTheme("tsparticles-docs", CustomTheme);
-}
--- utils/typedoc-theme/src/theme.tsx
+++ utils/typedoc-theme/src/theme.tsx
@@ -1,37 +1,44 @@
-import { DefaultThemeRenderContext, JSX, DefaultTheme } from "typedoc";
+import { DefaultThemeRenderContext, /*JSX,*/ DefaultTheme, Options } from "typedoc";
 
 class CustomThemeContext extends DefaultThemeRenderContext {
-  // Important: If you use `this`, this function MUST be bound! Template functions are free
-  // to destructure the context object to only grab what they care about.
-  override analytics = () => {
-    const oldRes = super.analytics ?? "";
+  //_oldAnalytics: DefaultThemeRenderContext["analytics"];
 
-    const clarityHtml = `(function(c,l,a,r,i,t,y){
-    c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
-    t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
-    y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
-})(window, document, "clarity", "script", "8q4bxin4tm");`;
+  constructor(theme: DefaultTheme, options: Options) {
+    super(theme, options);
 
-    return (
-      <div>
-        {oldRes}
-        <script type="text/javascript">
-          <JSX.Raw html={clarityHtml} />
-        </script>
-        <script
-          async
-          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1784552607103901"
-          crossOrigin="anonymous"
-        ></script>
-      </div>
-    );
-  };
+    /*this._oldAnalytics = this.analytics;
+        
+            this.analytics = () => {
+              const oldAnalytics = this._oldAnalytics();
+              const clarityHtml = `(function(c,l,a,r,i,t,y){
+                c[a]=c[a]||function(){(c[a].q=c[a].q||[]).push(arguments)};
+                t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+                y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+            })(window, document, "clarity", "script", "8q4bxin4tm");`;
+        
+              return (
+                <div>
+                  {oldAnalytics}
+                  <script type="text/javascript">
+                    <JSX.Raw html={clarityHtml} />
+                  </script>
+                  <script
+                    async
+                    src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-1784552607103901"
+                    crossOrigin="anonymous"
+                  ></script>
+                </div>
+              );
+            };*/
+  }
 }
 
 export class CustomTheme extends DefaultTheme {
   private _contextCache?: CustomThemeContext;
+
   override getRenderContext(): CustomThemeContext {
     this._contextCache ||= new CustomThemeContext(this, this.application.options);
+
     return this._contextCache;
   }
 }
