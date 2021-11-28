94f609bf4571c46d545d4e7c0c275b3e37fb5253
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2021-11-28 20:22:53 +0100

    build: fixed collisions

---
+++ interactions/particles/collisions/src/ResolveCollision.ts
@@ -0,0 +1,22 @@
+import type { Particle } from "tsparticles-engine";
+import { CollisionMode } from "tsparticles-engine";
+import { absorb } from "./Absorb";
+import { bounce } from "./Bounce";
+import { destroy } from "./Destroy";
+
+export function resolveCollision(p1: Particle, p2: Particle, fps: number, pixelRatio: number): void {
+    switch (p1.options.collisions.mode) {
+        case CollisionMode.absorb: {
+            absorb(p1, p2, fps, pixelRatio);
+            break;
+        }
+        case CollisionMode.bounce: {
+            bounce(p1, p2);
+            break;
+        }
+        case CollisionMode.destroy: {
+            destroy(p1, p2);
+            break;
+        }
+    }
+}
