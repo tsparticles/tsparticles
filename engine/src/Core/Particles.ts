9b70b786bd93ba7c245b8e24113156f747dd6608
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-03-20 13:12:39 +0100

    fix: moving canvas.clear closer to drawn breaks grab links, and maybe other interactions

--- engine/src/Core/Particles.ts
+++ engine/src/Core/Particles.ts
@@ -247,6 +247,9 @@ export class Particles {
             4
         );
 
+        /* clear canvas */
+        container.canvas.clear();
+
         /* update each particles param */
         await this.update(delta);
 
@@ -255,9 +258,6 @@ export class Particles {
             this.lastZIndex = this.zArray[this.zArray.length - 1].position.z;
             this.needsSort = false;
         }
-
-        /* clear canvas */
-        container.canvas.clear();
 
         /* draw polygon shape in debug mode */
         for (const [, plugin] of container.plugins) {
