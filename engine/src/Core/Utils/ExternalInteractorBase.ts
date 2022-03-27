e139504fee264f87b0c5195e4d03911a39f93eb5
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-03-27 21:17:50 +0200

    build: more documentation

--- engine/src/Core/Utils/ExternalInteractorBase.ts
+++ engine/src/Core/Utils/ExternalInteractorBase.ts
@@ -3,14 +3,36 @@ import type { Container } from "../Container";
 import { InteractorType } from "../../Enums";
 import type { Particle } from "../Particle";
 
+/**
+ * External Interactivity manager, base abstract class
+ */
 export abstract class ExternalInteractorBase implements IExternalInteractor {
+    /**
+     * Constructor of external interactivity manager
+     * @param container
+     * @protected
+     */
     protected constructor(protected readonly container: Container) {}
 
+    /**
+     * External Interactivity type
+     */
     type: InteractorType = InteractorType.External;
 
+    /**
+     * Interaction handler
+     * @param delta
+     */
     abstract interact(delta: IDelta): Promise<void>;
 
+    /**
+     * Interaction enabled check
+     */
     abstract isEnabled(): boolean;
 
+    /**
+     * Before interaction reset
+     * @param particle the particle to be reset
+     */
     abstract reset(particle: Particle): void;
 }
