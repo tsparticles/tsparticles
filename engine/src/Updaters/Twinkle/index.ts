62fea9579e894e40a3e57db7ad93a7ac31a5111f
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-04-06 11:38:00 +0200

    build: fixed twinkle updater, wrong id

--- engine/src/Updaters/Twinkle/index.ts
+++ engine/src/Updaters/Twinkle/index.ts
@@ -2,5 +2,5 @@ import type { Engine } from "../../engine";
 import { TwinkleUpdater } from "./TwinkleUpdater";
 
 export async function loadTwinkleUpdater(engine: Engine): Promise<void> {
-    await engine.addParticleUpdater("tilt", () => new TwinkleUpdater());
+    await engine.addParticleUpdater("twinkle", () => new TwinkleUpdater());
 }
