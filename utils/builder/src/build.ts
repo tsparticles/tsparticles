1989333e0ff05cc2352d78f8b6d1b77ef899644b
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-07-17 23:30:02 +0200

    build: started using the new build project, still some issues and unfinished tasks but right way

--- utils/builder/src/build.ts
+++ utils/builder/src/build.ts
@@ -1,13 +1,100 @@
 #!/usr/bin/env node
+import * as ts from "typescript";
 import { ESLint } from "eslint";
 import fs from "fs-extra";
 import klaw from "klaw";
 import path from "path";
 import prettier from "prettier";
-//import tsc from "typescript";
 
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const pkgInfo = require("../package.json");
+
+function compile(basePath: string, type: "browser" | "cjs" | "esm" | "types" | "umd"): number {
+    let options: unknown = undefined;
+
+    switch (type) {
+        case "browser":
+            options = {
+                extends: "@tsparticles/tsconfig/tsconfig.browser.json",
+                compilerOptions: {
+                    rootDir: "./src",
+                    outDir: "./dist/browser",
+                },
+                include: ["./src"],
+            };
+            break;
+        case "cjs":
+            options = {
+                extends: "@tsparticles/tsconfig/tsconfig.json",
+                compilerOptions: {
+                    rootDir: "./src",
+                    outDir: "./dist/cjs",
+                },
+                include: ["./src"],
+            };
+            break;
+        case "esm":
+            options = {
+                extends: "@tsparticles/tsconfig/tsconfig.module.json",
+                compilerOptions: {
+                    rootDir: "./src",
+                    outDir: "./dist/esm",
+                },
+                include: ["./src"],
+            };
+            break;
+        case "types":
+            options = {
+                extends: "@tsparticles/tsconfig/tsconfig.types.json",
+                compilerOptions: {
+                    rootDir: "./src",
+                    outDir: "./dist/types",
+                },
+                include: ["./src"],
+            };
+            break;
+        case "umd":
+            options = {
+                extends: "@tsparticles/tsconfig/tsconfig.umd.json",
+                compilerOptions: {
+                    rootDir: "./src",
+                    outDir: "./dist/umd",
+                },
+                include: ["./src"],
+            };
+            break;
+    }
+
+    if (!options) {
+        return 0;
+    }
+
+    const parsed = ts.parseJsonConfigFileContent(options, ts.sys, basePath);
+
+    if (!parsed) {
+        return 0;
+    }
+
+    const program = ts.createProgram(parsed.fileNames, parsed.options),
+        emitResult = program.emit(),
+        allDiagnostics = ts.getPreEmitDiagnostics(program).concat(emitResult.diagnostics);
+
+    allDiagnostics.forEach((diagnostic) => {
+        if (diagnostic.file) {
+            const { line, character } = ts.getLineAndCharacterOfPosition(diagnostic.file, diagnostic.start ?? 0);
+            const message = ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n");
+            console.log(`${diagnostic.file.fileName} (${line + 1},${character + 1}): ${message}`);
+        } else {
+            console.log(ts.flattenDiagnosticMessageText(diagnostic.messageText, "\n"));
+        }
+    });
+
+    const exitCode = emitResult.emitSkipped ? 1 : 0;
+
+    console.log(`Process exiting with code '${exitCode}'.`);
+
+    return exitCode;
+}
 
 (async function (): Promise<void> {
     console.log(`tsParticles Builder v${pkgInfo.version}\n`);
@@ -41,7 +128,8 @@ const pkgInfo = require("../package.json");
         return;
     }
 
-    const srcPath = path.join(process.cwd(), process.argv[2]);
+    const basePath = process.cwd();
+    const srcPath = path.join(basePath, process.argv[2]);
 
     if (!(await fs.pathExists(srcPath))) {
         console.error("Provided path does not exist");
@@ -53,14 +141,27 @@ const pkgInfo = require("../package.json");
 
     // prettier
     for await (const file of klaw(srcPath)) {
+        console.log(file.path);
+
+        if (file.stats.isDirectory()) {
+            continue;
+        }
+
         const contents = await fs.readFile(file.path, "utf8");
 
-        prettier.resolveConfig(srcPath).then(async (options) => {
-            const formatted = prettier.format(contents, options || {});
+        const options = (await prettier.resolveConfig(basePath)) ?? {};
 
-            await fs.writeFile(file.path, formatted, "utf8");
-        });
+        options.printWidth = 120;
+        options.endOfLine = "lf";
+        options.parser = "typescript";
+        options.tabWidth = 4;
+
+        const formatted = prettier.format(contents, options);
+
+        await fs.writeFile(file.path, formatted, "utf8");
     }
+
+    console.log("prettier done");
 
     // eslint
     const eslint = new ESLint({
@@ -81,6 +182,14 @@ const pkgInfo = require("../package.json");
     console.log(resultText);
 
     // tsc
+    let exitCode = compile(basePath, "browser");
+
+    exitCode ||= compile(basePath, "cjs");
+    exitCode ||= compile(basePath, "esm");
+    exitCode ||= compile(basePath, "types");
+    exitCode ||= compile(basePath, "umd");
+
+    process.exit(exitCode);
 })().catch((error) => {
     process.exitCode = 1;
 
