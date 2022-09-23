0c95857bd47b379772d60ff9c15fc3799298345e
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-09-23 16:00:35 +0200

    build: updated wordpress authors

--- components/wordpress/wordpress-particles.php
+++ components/wordpress/wordpress-particles.php
@@ -7,8 +7,8 @@
  * Requires PHP:      7.0
  * Version:           2.3.3
  * Author:            matteobruni
- * License:           MIT
- * License URI:       https://mit-license.org
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       tsparticles-block
  *
  * @package           tsparticles-block
