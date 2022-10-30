c1f28855561cab39c22f8f55a316f7e856e2120b
codefactor-io <support@codefactor.io>
2022-10-30 23:52:38 +0000

    [CodeFactor] Apply fixes to commit ae82faa
    [ci skip] [skip ci]

--- components/vue3-alt/src/components/Particles.vue
+++ components/vue3-alt/src/components/Particles.vue
@@ -1,5 +1,5 @@
 <template>
-    <div :id="id"></div>
+  <div :id="id"></div>
 </template>
 
 <script lang="ts">
@@ -14,58 +14,58 @@ export type IParticlesParams = IParticlesProps;
 let container: Container | undefined;
 
 export default defineComponent({
-    name: "Particles",
-    props: {
-        id: {
-            type: String,
-            required: true,
-        },
-        options: {
-            type: Object as PropType<IParticlesProps>,
-        },
-        url: {
-            type: String,
-        },
-        particlesLoaded: {
-            type: Function as PropType<(container: Container) => void>,
-        },
-        particlesInit: {
-            type: Function as PropType<(engine: Engine) => void>,
-        },
+  name: "Particles",
+  props: {
+    id: {
+      type: String,
+      required: true,
     },
-    mounted(): void {
-        nextTick(async () => {
-            if (!this.id) {
-                throw new Error("Prop 'id' is required!");
-            }
-
-            tsParticles.init();
-
-            if (this.particlesInit) {
-                await this.particlesInit(tsParticles);
-            }
-
-            const cb = (cbContainer?: Container) => {
-                container = cbContainer;
-
-                if (this.particlesLoaded && container) {
-                    this.particlesLoaded(container);
-                }
-            };
-
-            const loadedContainer = await (this.url
-                ? tsParticles.loadJSON(this.id, this.url)
-                : tsParticles.load(this.id, this.options ?? {}));
-
-            cb(loadedContainer);
-        });
+    options: {
+      type: Object as PropType<IParticlesProps>,
     },
-    unmounted(): void {
-        if (container) {
-            container.destroy();
+    url: {
+      type: String,
+    },
+    particlesLoaded: {
+      type: Function as PropType<(container: Container) => void>,
+    },
+    particlesInit: {
+      type: Function as PropType<(engine: Engine) => void>,
+    },
+  },
+  mounted(): void {
+    nextTick(async () => {
+      if (!this.id) {
+        throw new Error("Prop 'id' is required!");
+      }
 
-            container = undefined;
+      tsParticles.init();
+
+      if (this.particlesInit) {
+        await this.particlesInit(tsParticles);
+      }
+
+      const cb = (cbContainer?: Container) => {
+        container = cbContainer;
+
+        if (this.particlesLoaded && container) {
+          this.particlesLoaded(container);
         }
-    },
+      };
+
+      const loadedContainer = await (this.url
+        ? tsParticles.loadJSON(this.id, this.url)
+        : tsParticles.load(this.id, this.options ?? {}));
+
+      cb(loadedContainer);
+    });
+  },
+  unmounted(): void {
+    if (container) {
+      container.destroy();
+
+      container = undefined;
+    }
+  },
 });
 </script>
