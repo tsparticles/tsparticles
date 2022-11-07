97d3f13c47f22575fb1b6d7ead6278983e111649
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-11-07 14:23:07 +0100

    build: improving links warp

--- interactions/particles/links/src/Utils.ts
+++ interactions/particles/links/src/Utils.ts
@@ -42,18 +42,32 @@ export function drawLinkLine(
             { x: canvasSize.width, y: 0 },
             { x: 0, y: canvasSize.height },
             { x: canvasSize.width, y: canvasSize.height },
+            { x: -canvasSize.width, y: 0 },
+            { x: 0, y: -canvasSize.height },
+            { x: -canvasSize.width, y: -canvasSize.height },
+            { x: canvasSize.width, y: -canvasSize.height },
+            { x: -canvasSize.width, y: canvasSize.height },
         ];
 
         let pi1: ICoordinates | undefined, pi2: ICoordinates | undefined;
 
         for (const offset of offsets) {
-            const pos2 = {
+            const pos1 = {
+                    x: begin.x + offset.x,
+                    y: begin.y + offset.y,
+                },
+                d1 = getDistances(pos1, end),
+                pos2 = {
                     x: end.x + offset.x,
                     y: end.y + offset.y,
                 },
-                d = getDistances(begin, pos2);
+                d2 = getDistances(begin, pos2);
 
-            if (d.dx === 0 && Math.abs(d.dy) <= maxDistance) {
+            if (d1.dx === 0 || d2.dx === 0) {
+                if (Math.abs(d1.dy) > maxDistance && Math.abs(d2.dy) > maxDistance) {
+                    continue;
+                }
+
                 if (begin.y > end.y) {
                     pi1 = { x: begin.x, y: canvasSize.height };
                     pi2 = { x: end.x, y: 0 };
@@ -63,7 +77,11 @@ export function drawLinkLine(
                 }
 
                 break;
-            } else if (d.dy === 0 && Math.abs(d.dx) <= maxDistance) {
+            } else if (d1.dy === 0 || d2.dy === 0) {
+                if (Math.abs(d1.dx) > maxDistance && Math.abs(d2.dx) > maxDistance) {
+                    continue;
+                }
+
                 if (begin.x > end.x) {
                     pi1 = { x: canvasSize.width, y: begin.y };
                     pi2 = { x: 0, y: end.y };
@@ -73,9 +91,14 @@ export function drawLinkLine(
                 }
 
                 break;
-            } else if (d.distance <= maxDistance) {
-                const m = d.dy / d.dx,
-                    q = begin.y - m * begin.x,
+            } else {
+                if (d1.distance > maxDistance && d2.distance > maxDistance) {
+                    continue;
+                }
+
+                const d = d1.distance <= maxDistance ? 1 : 2,
+                    m = d === 1 ? d1.dy / d1.dx : d2.dy / d2.dx,
+                    q = d === 1 ? end.y - m * end.x : begin.y - m * begin.x,
                     px0 = {
                         x: 0,
                         y: q,
@@ -91,11 +114,13 @@ export function drawLinkLine(
                     pyMax = {
                         x: (canvasSize.height - q) / m,
                         y: canvasSize.height,
-                    };
+                    },
+                    beginPos = d === 1 ? pos1 : begin,
+                    endPos = d === 1 ? end : pos2;
 
-                if (isPointBetweenPoints(py0, begin, pos2)) {
-                    const db = getDistance(begin, py0),
-                        de = getDistance(pos2, py0);
+                if (isPointBetweenPoints(py0, beginPos, endPos)) {
+                    const db = getDistance(beginPos, py0),
+                        de = getDistance(endPos, py0);
 
                     if (db < de) {
                         pi1 = py0;
@@ -104,9 +129,9 @@ export function drawLinkLine(
                         pi1 = { x: canvasSize.width, y: m * canvasSize.width + q };
                         pi2 = py0;
                     }
-                } else if (isPointBetweenPoints(px0, begin, pos2)) {
-                    const db = getDistance(begin, px0),
-                        de = getDistance(pos2, px0);
+                } else if (isPointBetweenPoints(px0, beginPos, endPos)) {
+                    const db = getDistance(beginPos, px0),
+                        de = getDistance(endPos, px0);
 
                     if (db < de) {
                         pi1 = px0;
@@ -115,9 +140,9 @@ export function drawLinkLine(
                         pi1 = { x: -q / m, y: canvasSize.height };
                         pi2 = px0;
                     }
-                } else if (isPointBetweenPoints(pyMax, begin, pos2)) {
-                    const db = getDistance(begin, pyMax),
-                        de = getDistance(pos2, pyMax);
+                } else if (isPointBetweenPoints(pyMax, beginPos, endPos)) {
+                    const db = getDistance(beginPos, pyMax),
+                        de = getDistance(endPos, pyMax);
 
                     if (db < de) {
                         pi1 = pyMax;
@@ -126,9 +151,9 @@ export function drawLinkLine(
                         pi1 = { x: 0, y: q };
                         pi2 = pyMax;
                     }
-                } else if (isPointBetweenPoints(pxMax, begin, pos2)) {
-                    const db = getDistance(begin, pxMax),
-                        de = getDistance(pos2, pxMax);
+                } else if (isPointBetweenPoints(pxMax, beginPos, endPos)) {
+                    const db = getDistance(beginPos, pxMax),
+                        de = getDistance(endPos, pxMax);
 
                     if (db < de) {
                         pi1 = pxMax;
@@ -139,7 +164,9 @@ export function drawLinkLine(
                     }
                 }
 
-                break;
+                if (pi1 && pi2) {
+                    break;
+                }
             }
         }
 
