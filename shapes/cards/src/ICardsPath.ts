5824dcc2d0ef217492229cdb54bd0da86493c8d6
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-10 01:43:18 +0100

    build: using object instead of anonymous arrays in card shape

--- shapes/cards/src/ICardsPath.ts
+++ shapes/cards/src/ICardsPath.ts
@@ -1,8 +1,16 @@
 import type { ICoordinates } from "tsparticles-engine";
 
+export interface IPathSegment {
+    values: ICoordinates[];
+}
+
+export interface IPath {
+    segments: IPathSegment[];
+}
+
 export interface ICardsPath {
-    readonly club: ICoordinates[][];
-    readonly diamond: ICoordinates[][];
-    readonly heart: ICoordinates[][];
-    readonly spade: ICoordinates[][];
+    readonly club: IPath;
+    readonly diamond: IPath;
+    readonly heart: IPath;
+    readonly spade: IPath;
 }
