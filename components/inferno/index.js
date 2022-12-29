63481ce09dc6000207862c94b3cf0e06410ecabf
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2022-12-29 12:49:21 +0100

    build: moved out inferno component and app to its repository

--- components/inferno/index.js
+++
@@ -1,7 +0,0 @@
-const InfernoParticles = require("./dist/particles");
-
-for (let key in InfernoParticles) {
-	InfernoParticles.default[key] = InfernoParticles[key];
-}
-
-module.exports = InfernoParticles.default;
--- demo/inferno/src/utils/math.ts
+++
@@ -1,5 +0,0 @@
-/* This is example of regular Typescript file used with JSX */
-
-export function addOne(value: number): number {
-	return value + 1;
-}
