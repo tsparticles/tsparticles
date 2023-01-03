e208af3a5bdcc474fa88618dd82c69b849683b55
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-01-03 09:50:38 +0100

    chore(engine): reworking animation options, for more common and reusable code

--- engine/src/Options/Classes/Particles/Opacity/Opacity.ts
+++ engine/src/Options/Classes/Particles/Opacity/Opacity.ts
@@ -1,19 +1,18 @@
 import type { IOpacity } from "../../../Interfaces/Particles/Opacity/IOpacity";
 import type { IOptionLoader } from "../../../Interfaces/IOptionLoader";
 import { OpacityAnimation } from "./OpacityAnimation";
-import type { RecursivePartial } from "../../../../Types/RecursivePartial";
-import { ValueWithRandom } from "../../ValueWithRandom";
-import { setRangeValue } from "../../../../Utils/NumberUtils";
+import { RangedAnimationValueWithRandom } from "../../ValueWithRandom";
 
 /**
  * [[include:Options/Particles/Opacity.md]]
  * @category Options
  */
-export class Opacity extends ValueWithRandom implements IOpacity, IOptionLoader<IOpacity> {
+export class Opacity extends RangedAnimationValueWithRandom implements IOpacity, IOptionLoader<IOpacity> {
     animation;
 
     constructor() {
         super();
+
         this.animation = new OpacityAnimation();
         this.random.minimumValue = 0.1;
         this.value = 1;
@@ -34,21 +33,5 @@ export class Opacity extends ValueWithRandom implements IOpacity, IOptionLoader<
      */
     set anim(value: OpacityAnimation) {
         this.animation = value;
-    }
-
-    load(data?: RecursivePartial<IOpacity>): void {
-        if (!data) {
-            return;
-        }
-
-        super.load(data);
-
-        const animation = data.animation ?? data.anim;
-
-        if (animation !== undefined) {
-            this.animation.load(animation);
-
-            this.value = setRangeValue(this.value, this.animation.enable ? this.animation.minimumValue : undefined);
-        }
     }
 }
