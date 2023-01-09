ce2c4e06791f9a4c7f9656d56065db3948186c67
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-01-09 09:03:14 +0100

    build: removed presets from demo

--- demo/vanilla/app.js
+++ demo/vanilla/app.js
@@ -102,18 +102,6 @@ app.use("/plugin-hsv-color", express.static("./node_modules/tsparticles-plugin-h
 app.use("/plugin-infection", express.static("./node_modules/tsparticles-plugin-infection"));
 app.use("/plugin-motion", express.static("./node_modules/tsparticles-plugin-motion"));
 app.use("/plugin-sounds", express.static("./node_modules/tsparticles-plugin-sounds"));
-app.use("/preset-big-circles", express.static("./node_modules/tsparticles-preset-big-circles"));
-app.use("/preset-bubbles", express.static("./node_modules/tsparticles-preset-bubbles"));
-app.use("/preset-confetti", express.static("./node_modules/tsparticles-preset-confetti"));
-app.use("/preset-fire", express.static("./node_modules/tsparticles-preset-fire"));
-app.use("/preset-firefly", express.static("./node_modules/tsparticles-preset-firefly"));
-app.use("/preset-fireworks", express.static("./node_modules/tsparticles-preset-fireworks"));
-app.use("/preset-fountain", express.static("./node_modules/tsparticles-preset-fountain"));
-app.use("/preset-links", express.static("./node_modules/tsparticles-preset-links"));
-app.use("/preset-sea-anemone", express.static("./node_modules/tsparticles-preset-sea-anemone"));
-app.use("/preset-snow", express.static("./node_modules/tsparticles-preset-snow"));
-app.use("/preset-stars", express.static("./node_modules/tsparticles-preset-stars"));
-app.use("/preset-triangles", express.static("./node_modules/tsparticles-preset-triangles"));
 app.use("/shape-bubble", express.static("./node_modules/tsparticles-shape-bubble"));
 app.use("/shape-cards", express.static("./node_modules/tsparticles-shape-cards"));
 app.use("/shape-heart", express.static("./node_modules/tsparticles-shape-heart"));
@@ -145,58 +133,6 @@ app.get('/click', function (req, res) {
 
 app.get('/noid', function (req, res) {
     res.render('noid');
-});
-
-app.get("/presets", function (req, res) {
-    res.render("presets/index");
-});
-
-app.get("/presets/bigCircles", function (req, res) {
-    res.render("presets/bigCircles");
-});
-
-app.get("/presets/bubbles", function (req, res) {
-    res.render("presets/bubbles");
-});
-
-app.get("/presets/confetti", function (req, res) {
-    res.render("presets/confetti");
-});
-
-app.get("/presets/fire", function (req, res) {
-    res.render("presets/fire");
-});
-
-app.get("/presets/firefly", function (req, res) {
-    res.render("presets/firefly");
-});
-
-app.get("/presets/fireworks", function (req, res) {
-    res.render("presets/fireworks");
-});
-
-app.get("/presets/fountain", function (req, res) {
-    res.render("presets/fountain");
-});
-
-app.get("/presets/links", function (req, res) {
-    res.render("presets/links");
-});
-
-app.get("/presets/seaAnemone", function (req, res) {
-    res.render("presets/seaAnemone");
-});
-
-app.get("/presets/snow", function (req, res) {
-    res.render("presets/snow");
-});
-
-app.get("/presets/stars", function (req, res) {
-    res.render("presets/stars");
-});
-
-app.get("/presets/triangles", function (req, res) {
-    res.render("presets/triangles");
 });
 
 app.listen(port, () => console.log(`Demo app listening on port ${port}!`));
