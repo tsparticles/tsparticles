670871683deb645fde69f781e7845648250efa2a
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-02-21 17:19:14 +0100

    feat: added max speed value to collisions options

--- engine/src/Options/Interfaces/Particles/Collisions/ICollisions.ts
+++ engine/src/Options/Interfaces/Particles/Collisions/ICollisions.ts
@@ -2,6 +2,7 @@ import type { CollisionMode } from "../../../../Enums/Modes/CollisionMode";
 import type { ICollisionsAbsorb } from "./ICollisionsAbsorb";
 import type { ICollisionsOverlap } from "./ICollisionsOverlap";
 import type { IParticlesBounce } from "../Bounce/IParticlesBounce";
+import type { RangeValue } from "../../../../Types/RangeValue";
 
 /**
  * @category Options
@@ -11,6 +12,7 @@ export interface ICollisions {
     absorb: ICollisionsAbsorb;
     bounce: IParticlesBounce;
     enable: boolean;
+    maxSpeed: RangeValue;
     mode: CollisionMode | keyof typeof CollisionMode;
     overlap: ICollisionsOverlap;
 }
--- interactions/particles/collisions/src/Bounce.ts
+++ interactions/particles/collisions/src/Bounce.ts
@@ -1,6 +1,23 @@
-import { circleBounce, circleBounceDataFromParticle } from "tsparticles-engine";
+import { circleBounce, circleBounceDataFromParticle, getRangeValue } from "tsparticles-engine";
 import type { Particle } from "tsparticles-engine";
 
-export function bounce(p1: Particle, p2: Particle): void {
+type BounceParticle = Particle & {
+    collisionMaxSpeed?: number;
+};
+
+const fixBounceSpeed = (p: BounceParticle): void => {
+    if (p.collisionMaxSpeed === undefined) {
+        p.collisionMaxSpeed = getRangeValue(p.options.collisions.maxSpeed);
+    }
+
+    if (p.velocity.length > p.collisionMaxSpeed) {
+        p.velocity.length = p.collisionMaxSpeed;
+    }
+};
+
+export function bounce(p1: BounceParticle, p2: BounceParticle): void {
     circleBounce(circleBounceDataFromParticle(p1), circleBounceDataFromParticle(p2));
+
+    fixBounceSpeed(p1);
+    fixBounceSpeed(p2);
 }
