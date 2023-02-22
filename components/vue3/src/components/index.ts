fcbb16a92503ae66e89b586670c05a6c00095e84
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-02-22 10:50:07 +0100

    build: removed all components and moved to their own repositories

--- components/vue3/src/components/index.ts
+++
@@ -1,9 +0,0 @@
-import type { App } from "vue";
-import particles from "./Particles.vue";
-
-const VueParticles = (app: App, options: unknown) => {
-    app.component("Particles", particles);
-};
-
-export { particles as ParticlesComponent };
-export default VueParticles;
--- demo/vue/babel.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    presets: [
-        [ '@vue/cli-plugin-babel/preset', {
-            "polyfills": [ "es7.object.entries", "es6.promise" ],
-            "useBuiltIns": "entry"
-        } ],
-    ]
-}
