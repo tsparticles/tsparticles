a23809a3014cc382c298021ac15b1d22372afad2
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-04-26 01:17:16 +0200

    build: removed debug console.log

--- plugins/sounds/src/utils.ts
+++ plugins/sounds/src/utils.ts
@@ -15,7 +15,8 @@ notes.set("B", [30.87, 61.74, 123.47, 246.94, 493.88, 987.77, 1975.53, 3951.07, 
 notes.set("pause", [0]);
 
 /**
- * @param note
+ * @param note -
+ * @returns frequency
  */
 export function getNoteFrequency(note: string): number | undefined {
     const regex = /(([A-G]b?)(\d))|pause/i,
@@ -24,8 +25,6 @@ export function getNoteFrequency(note: string): number | undefined {
     if (!result || !result.length) {
         return;
     }
-
-    console.log(result);
 
     const noteKey = result[2] || result[0],
         noteItem = notes.get(noteKey);
