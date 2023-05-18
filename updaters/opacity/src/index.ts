a8e7d6925bb6566b6e7d9523a32a05c9d118ecc8
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-05-18 08:36:45 +0200

    build: fixed some comments

--- updaters/opacity/src/index.ts
+++ updaters/opacity/src/index.ts
@@ -2,7 +2,7 @@ import type { Engine } from "tsparticles-engine";
 import { OpacityUpdater } from "./OpacityUpdater";
 
 /**
- * @param engine
+ * @param engine - The engine instance to load the updater for
  */
 export async function loadOpacityUpdater(engine: Engine): Promise<void> {
     await engine.addParticleUpdater("opacity", (container) => new OpacityUpdater(container));
