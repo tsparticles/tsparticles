41f0a2a5ff613ac097bc6189175db4c70dfb4c6f
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-06-01 19:10:34 +0200

    build: common dev dependencies in project root package.json

--- demo/vanilla/app.ts
+++ demo/vanilla/app.ts
@@ -37,7 +37,6 @@ app.use("/jsoneditor", express.static("./node_modules/jsoneditor/dist"));
 app.use("/jquery", express.static("./node_modules/jquery/dist"));
 app.use("/lodash", express.static("./node_modules/lodash"));
 app.use("/ace", express.static("./node_modules/ace-builds"));
-app.use("/popper.js", express.static("./node_modules/popper.js/dist"));
 app.use("/bootstrap", express.static("./node_modules/bootstrap/dist"));
 app.use("/tsparticles-engine", express.static("./node_modules/tsparticles-engine"));
 app.use("/tsparticles-particles.js", express.static("./node_modules/tsparticles-particles.js"));
