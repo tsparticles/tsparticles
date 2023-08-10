f1c15ab07e305812f784c57e28877357ecfc3968
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-08-10 09:51:26 +0200

    build: added deploy script for docs.json

---
+++ deploy.docs-json.js
@@ -0,0 +1,24 @@
+const ghpages = require("gh-pages");
+const path = require("path");
+
+const ghToken = process.env.GITHUB_TOKEN, gitUser = ghToken ? {
+    name: "github-actions-bot", email: "support+actions@github.com",
+} : {
+    name: "Matteo Bruni", email: "176620+matteobruni@users.noreply.github.com",
+};
+
+ghpages.publish(path.resolve("."), {
+    src: "docs.json",
+    repo: ghToken ? `https://git:${ghToken}@github.com/matteobruni/tsparticles.git` : `https://github.com/matteobruni/tsparticles.git`,
+    branch: "docs-gen",
+    dotfiles: true,
+    history: false,
+    message: "build: docs-gen updated",
+    user: gitUser,
+}, function(publishErr) {
+    if (!publishErr) {
+        console.log("Docs JSON published successfully");
+    } else {
+        console.log(`Error publishing Docs JSON: ${publishErr}`);
+    }
+});
