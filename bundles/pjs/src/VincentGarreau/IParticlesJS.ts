ec70f72baa86f6f5bf5e427f65a91e2f6ef5d999
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-11-29 02:28:23 +0100

    chore(pjs): adding full support of vg pjs options, since it was broken in v3

--- bundles/pjs/src/VincentGarreau/IParticlesJS.ts
+++ bundles/pjs/src/VincentGarreau/IParticlesJS.ts
@@ -1,4 +1,124 @@
-import type { Container, ISourceOptions } from "@tsparticles/engine";
+import type {
+    Container,
+    IHsl,
+    IRgb,
+    IValueColor,
+    InteractivityDetect,
+    MoveDirection,
+    OutMode,
+} from "@tsparticles/engine";
+
+export interface IParticlesJSOptions {
+    interactivity: {
+        detect_on: InteractivityDetect | keyof typeof InteractivityDetect;
+        events: {
+            onclick: {
+                enable: boolean;
+                mode: string;
+            };
+            onhover: {
+                enable: boolean;
+                mode: string;
+            };
+            resize: boolean;
+        };
+        modes: {
+            bubble: {
+                distance: number;
+                duration: number;
+                opacity: number;
+                size: number;
+                speed: number;
+            };
+            grab: {
+                distance: number;
+                line_linked: {
+                    opacity: number;
+                };
+            };
+            push: {
+                particles_nb: number;
+            };
+            remove: {
+                particles_nb: number;
+            };
+            repulse: {
+                distance: number;
+                duration: number;
+            };
+        };
+    };
+    particles: {
+        color: {
+            value: string | IRgb | IHsl | IValueColor;
+        };
+        line_linked: {
+            color: string;
+            distance: number;
+            enable: boolean;
+            opacity: number;
+            width: number;
+        };
+        move: {
+            attract: {
+                enable: boolean;
+                rotateX: number;
+                rotateY: number;
+            };
+            bounce: boolean;
+            direction: MoveDirection | keyof typeof MoveDirection;
+            enable: boolean;
+            out_mode: OutMode | keyof typeof OutMode;
+            random: boolean;
+            speed: number;
+            straight: boolean;
+        };
+        number: {
+            density: {
+                enable: boolean;
+                value_area: number;
+            };
+            value: number;
+        };
+        opacity: {
+            anim: {
+                enable: boolean;
+                opacity_min: number;
+                speed: number;
+                sync: boolean;
+            };
+            random: boolean;
+            value: number;
+        };
+        shape: {
+            image: {
+                height: number;
+                replace_color?: boolean;
+                src: string;
+                width: number;
+            };
+            polygon: {
+                nb_sides: number;
+            };
+            stroke: {
+                color: string | IRgb | IHsl | IValueColor;
+                width: number;
+            };
+            type: string;
+        };
+        size: {
+            anim: {
+                enable: boolean;
+                size_min: number;
+                speed: number;
+                sync: boolean;
+            };
+            random: boolean;
+            value: number;
+        };
+    };
+    retina_detect: boolean;
+}
 
 /**
  * [[include:pjsMigration.md]]
@@ -10,7 +130,7 @@ export interface IParticlesJS {
      * @param tagId - the particles container element id
      * @param options - the options object to initialize the {@link Container}
      */
-    (tagId: string, options: ISourceOptions): Promise<Container | undefined>;
+    (tagId: string, options: IParticlesJSOptions): Promise<Container | undefined>;
 
     /**
      * Loads the provided json with a GET request.
--- bundles/pjs/src/VincentGarreau/particles.ts
+++ bundles/pjs/src/VincentGarreau/particles.ts
@@ -1,5 +1,116 @@
-import { type Container, type Engine, type ISourceOptions, type Particle } from "@tsparticles/engine";
-import type { IParticlesJS } from "./IParticlesJS.js";
+import { type Container, type Engine, type Particle, type RecursivePartial, deepExtend } from "@tsparticles/engine";
+import type { IParticlesJS, IParticlesJSOptions } from "./IParticlesJS.js";
+
+const defaultPjsOptions: IParticlesJSOptions = {
+    particles: {
+        number: {
+            value: 400,
+            density: {
+                enable: true,
+                value_area: 800,
+            },
+        },
+        color: {
+            value: "#fff",
+        },
+        shape: {
+            type: "circle",
+            stroke: {
+                width: 0,
+                color: "#ff0000",
+            },
+            polygon: {
+                nb_sides: 5,
+            },
+            image: {
+                src: "",
+                width: 100,
+                height: 100,
+            },
+        },
+        opacity: {
+            value: 1,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 2,
+                opacity_min: 0,
+                sync: false,
+            },
+        },
+        size: {
+            value: 20,
+            random: false,
+            anim: {
+                enable: false,
+                speed: 20,
+                size_min: 0,
+                sync: false,
+            },
+        },
+        line_linked: {
+            enable: true,
+            distance: 100,
+            color: "#fff",
+            opacity: 1,
+            width: 1,
+        },
+        move: {
+            enable: true,
+            speed: 2,
+            direction: "none",
+            random: false,
+            straight: false,
+            out_mode: "out",
+            bounce: false,
+            attract: {
+                enable: false,
+                rotateX: 3000,
+                rotateY: 3000,
+            },
+        },
+    },
+    interactivity: {
+        detect_on: "canvas",
+        events: {
+            onhover: {
+                enable: true,
+                mode: "grab",
+            },
+            onclick: {
+                enable: true,
+                mode: "push",
+            },
+            resize: true,
+        },
+        modes: {
+            grab: {
+                distance: 100,
+                line_linked: {
+                    opacity: 1,
+                },
+            },
+            bubble: {
+                distance: 200,
+                size: 80,
+                duration: 0.4,
+                opacity: 1,
+                speed: 3,
+            },
+            repulse: {
+                distance: 200,
+                duration: 0.4,
+            },
+            push: {
+                particles_nb: 4,
+            },
+            remove: {
+                particles_nb: 2,
+            },
+        },
+    },
+    retina_detect: false,
+};
 
 const initParticlesJS = (
     engine: Engine,
@@ -23,8 +134,147 @@ const initParticlesJS = (
      * @param options - the options object to initialize the {@link Container}
      * @returns the loaded container
      */
-    const particlesJS = (tagId: string, options: ISourceOptions): Promise<Container | undefined> => {
-        return engine.load({ id: tagId, options });
+    const particlesJS = (
+        tagId: string,
+        options: RecursivePartial<IParticlesJSOptions>,
+    ): Promise<Container | undefined> => {
+        const fixedOptions = deepExtend(defaultPjsOptions, options) as IParticlesJSOptions;
+
+        return engine.load({
+            id: tagId,
+            options: {
+                fullScreen: {
+                    enable: false,
+                },
+                detectRetina: fixedOptions.retina_detect,
+                smooth: true,
+                interactivity: {
+                    detectsOn: fixedOptions.interactivity.detect_on,
+                    events: {
+                        onHover: {
+                            enable: fixedOptions.interactivity.events.onhover.enable,
+                            mode: fixedOptions.interactivity.events.onhover.mode,
+                        },
+                        onClick: {
+                            enable: fixedOptions.interactivity.events.onclick.enable,
+                            mode: fixedOptions.interactivity.events.onclick.mode,
+                        },
+                        resize: fixedOptions.interactivity.events.resize,
+                    },
+                    modes: {
+                        grab: {
+                            distance: fixedOptions.interactivity.modes.grab.distance,
+                            lineLinked: {
+                                opacity: fixedOptions.interactivity.modes.grab.line_linked.opacity,
+                            },
+                        },
+                        bubble: {
+                            distance: fixedOptions.interactivity.modes.bubble.distance,
+                            size: fixedOptions.interactivity.modes.bubble.size,
+                            duration: fixedOptions.interactivity.modes.bubble.duration,
+                            opacity: fixedOptions.interactivity.modes.bubble.opacity,
+                            speed: fixedOptions.interactivity.modes.bubble.speed,
+                        },
+                        repulse: {
+                            distance: fixedOptions.interactivity.modes.repulse.distance,
+                            duration: fixedOptions.interactivity.modes.repulse.duration,
+                        },
+                        push: {
+                            particles_nb: fixedOptions.interactivity.modes.push.particles_nb,
+                        },
+                        remove: {
+                            particles_nb: fixedOptions.interactivity.modes.remove.particles_nb,
+                        },
+                    },
+                },
+                particles: {
+                    collisions: {
+                        enable: fixedOptions.particles.move.bounce,
+                    },
+                    number: {
+                        value: fixedOptions.particles.number.value,
+                        density: {
+                            enable: fixedOptions.particles.number.density.enable,
+                            width: fixedOptions.particles.number.density.value_area,
+                        },
+                    },
+                    color: {
+                        value: fixedOptions.particles.color.value,
+                    },
+                    stroke: {
+                        width: fixedOptions.particles.shape.stroke.width,
+                        color: {
+                            value: fixedOptions.particles.shape.stroke.color,
+                        },
+                    },
+                    shape: {
+                        type: fixedOptions.particles.shape.type,
+                        options: {
+                            polygon: {
+                                nb_sides: fixedOptions.particles.shape.polygon.nb_sides,
+                            },
+                            image: {
+                                src: fixedOptions.particles.shape.image.src,
+                                width: fixedOptions.particles.shape.image.width,
+                                height: fixedOptions.particles.shape.image.height,
+                            },
+                        },
+                    },
+                    opacity: {
+                        value: fixedOptions.particles.opacity.random
+                            ? {
+                                  min: fixedOptions.particles.opacity.anim.enable
+                                      ? fixedOptions.particles.opacity.anim.opacity_min
+                                      : 0,
+                                  max: fixedOptions.particles.opacity.value,
+                              }
+                            : fixedOptions.particles.opacity.value,
+                        animation: {
+                            enable: fixedOptions.particles.opacity.anim.enable,
+                            speed: fixedOptions.particles.opacity.anim.speed,
+                            sync: fixedOptions.particles.opacity.anim.sync,
+                        },
+                    },
+                    size: {
+                        value: fixedOptions.particles.size.random
+                            ? {
+                                  min: fixedOptions.particles.size.anim.enable
+                                      ? fixedOptions.particles.size.anim.size_min
+                                      : 0,
+                                  max: fixedOptions.particles.size.value,
+                              }
+                            : fixedOptions.particles.size.value,
+                        animation: {
+                            enable: fixedOptions.particles.size.anim.enable,
+                            speed: fixedOptions.particles.size.anim.speed,
+                            sync: fixedOptions.particles.size.anim.sync,
+                        },
+                    },
+                    links: {
+                        enable: fixedOptions.particles.line_linked.enable,
+                        distance: fixedOptions.particles.line_linked.distance,
+                        color: fixedOptions.particles.line_linked.color,
+                        opacity: fixedOptions.particles.line_linked.opacity,
+                        width: fixedOptions.particles.line_linked.width,
+                    },
+                    move: {
+                        enable: fixedOptions.particles.move.enable,
+                        speed: fixedOptions.particles.move.speed / 3,
+                        direction: fixedOptions.particles.move.direction,
+                        random: fixedOptions.particles.move.random,
+                        straight: fixedOptions.particles.move.straight,
+                        outModes: fixedOptions.particles.move.out_mode,
+                        attract: {
+                            enable: fixedOptions.particles.move.attract.enable,
+                            rotate: {
+                                x: fixedOptions.particles.move.attract.rotateX,
+                                y: fixedOptions.particles.move.attract.rotateY,
+                            },
+                        },
+                    },
+                },
+            },
+        });
     };
 
     /**
