cbb5b762a4a2040c10969d8b26882976e0413b62
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-12-02 02:36:25 +0100

    chore(engine): improving options

--- bundles/pjs/src/VincentGarreau/IParticlesJS.ts
+++ bundles/pjs/src/VincentGarreau/IParticlesJS.ts
@@ -135,7 +135,7 @@ export interface IParticlesJS {
     /**
      * Loads the provided json with a GET request.
      * The content will be used to create a {@link Container} object.
-     * @deprecated this method is obsolete, please use the new tsParticles.loadJSON
+     * @deprecated this method is obsolete, please use the new tsParticles.load
      * @param tagId - the particles container element id
      * @param pathConfigJson - the json path to use in the GET request
      * @param callback - called after the {@link Container} is loaded and it will be passed as a parameter
--- bundles/pjs/src/VincentGarreau/particles.ts
+++ bundles/pjs/src/VincentGarreau/particles.ts
@@ -282,7 +282,7 @@ const initParticlesJS = (
     /**
      * Loads the provided json with a GET request.
      * The content will be used to create a {@link Container} object.
-     * @deprecated this method is obsolete, please use the new tsParticles.loadJSON
+     * @deprecated this method is obsolete, please use the new tsParticles.load
      * @param tagId - the particles container element id
      * @param pathConfigJson - the json path to use in the GET request
      * @param callback - called after the {@link Container} is loaded and it will be passed as a parameter
