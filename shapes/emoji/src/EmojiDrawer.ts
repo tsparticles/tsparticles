3d84380b45cbbc3f01d39deb069d37ce54268c68
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2023-12-26 19:10:12 +0100

    fix: fixed memory allocation of emoji shape

--- shapes/emoji/src/EmojiDrawer.ts
+++ shapes/emoji/src/EmojiDrawer.ts
@@ -19,9 +19,11 @@ export class EmojiDrawer implements IShapeDrawer<EmojiParticle> {
     private readonly _emojiShapeDict: Map<string, ImageBitmap | HTMLCanvasElement> = new Map<string, ImageBitmap>();
 
     destroy(): void {
-        for (const [, emojiData] of this._emojiShapeDict) {
+        for (const [key, emojiData] of this._emojiShapeDict) {
             if (emojiData instanceof ImageBitmap) {
                 emojiData?.close();
+
+                this._emojiShapeDict.delete(key);
             }
         }
     }
@@ -70,76 +72,73 @@ export class EmojiDrawer implements IShapeDrawer<EmojiParticle> {
     }
 
     particleInit(container: Container, particle: EmojiParticle): void {
-        const double = 2;
+        const double = 2,
+            shapeData = particle.shapeData as unknown as IEmojiShape;
 
-        if (!particle.emojiData) {
-            const shapeData = particle.shapeData as unknown as IEmojiShape;
-
-            if (!shapeData?.value) {
-                return;
-            }
-
-            const emoji = itemFromSingleOrMultiple(shapeData.value, particle.randomIndexData),
-                font = shapeData.font ?? defaultFont;
-
-            if (!emoji) {
-                return;
-            }
-
-            const key = `${emoji}_${font}`,
-                existingData = this._emojiShapeDict.get(key);
-
-            if (existingData) {
-                particle.emojiData = existingData;
-
-                return;
-            }
-
-            const canvasSize = getRangeMax(particle.size.value) * double;
-
-            let emojiData: ImageBitmap | HTMLCanvasElement;
-
-            const maxSize = getRangeMax(particle.size.value);
-
-            if (typeof OffscreenCanvas !== "undefined") {
-                const canvas = new OffscreenCanvas(canvasSize, canvasSize),
-                    context = canvas.getContext("2d");
-
-                if (!context) {
-                    return;
-                }
-
-                context.font = `400 ${maxSize * double}px ${font}`;
-                context.textBaseline = "middle";
-                context.textAlign = "center";
-
-                context.fillText(emoji, maxSize, maxSize);
-
-                emojiData = canvas.transferToImageBitmap();
-            } else {
-                const canvas = document.createElement("canvas");
-
-                canvas.width = canvasSize;
-                canvas.height = canvasSize;
-
-                const context = canvas.getContext("2d");
-
-                if (!context) {
-                    return;
-                }
-
-                context.font = `400 ${maxSize * double}px ${font}`;
-                context.textBaseline = "middle";
-                context.textAlign = "center";
-
-                context.fillText(emoji, maxSize, maxSize);
-
-                emojiData = canvas;
-            }
-
-            this._emojiShapeDict.set(key, emojiData);
-
-            particle.emojiData = emojiData;
+        if (!shapeData?.value) {
+            return;
         }
+
+        const emoji = itemFromSingleOrMultiple(shapeData.value, particle.randomIndexData),
+            font = shapeData.font ?? defaultFont;
+
+        if (!emoji) {
+            return;
+        }
+
+        const key = `${emoji}_${font}`,
+            existingData = this._emojiShapeDict.get(key);
+
+        if (existingData) {
+            particle.emojiData = existingData;
+
+            return;
+        }
+
+        const canvasSize = getRangeMax(particle.size.value) * double;
+
+        let emojiData: ImageBitmap | HTMLCanvasElement;
+
+        const maxSize = getRangeMax(particle.size.value);
+
+        if (typeof OffscreenCanvas !== "undefined") {
+            const canvas = new OffscreenCanvas(canvasSize, canvasSize),
+                context = canvas.getContext("2d");
+
+            if (!context) {
+                return;
+            }
+
+            context.font = `400 ${maxSize * double}px ${font}`;
+            context.textBaseline = "middle";
+            context.textAlign = "center";
+
+            context.fillText(emoji, maxSize, maxSize);
+
+            emojiData = canvas.transferToImageBitmap();
+        } else {
+            const canvas = document.createElement("canvas");
+
+            canvas.width = canvasSize;
+            canvas.height = canvasSize;
+
+            const context = canvas.getContext("2d");
+
+            if (!context) {
+                return;
+            }
+
+            context.font = `400 ${maxSize * double}px ${font}`;
+            context.textBaseline = "middle";
+            context.textAlign = "center";
+
+            context.fillText(emoji, maxSize, maxSize);
+
+            emojiData = canvas;
+        }
+
+        this._emojiShapeDict.set(key, emojiData);
+
+        particle.emojiData = emojiData;
     }
 }
