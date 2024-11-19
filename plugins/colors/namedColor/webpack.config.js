20df6f906dfc85a6c1ad9067eaaa9711ad59ac1e
codefactor-io <support@codefactor.io>
2024-11-19 11:15:57 +0000

    [CodeFactor] Apply fixes
    [ci skip] [skip ci]

--- plugins/colors/namedColor/webpack.config.js
+++ plugins/colors/namedColor/webpack.config.js
@@ -1,4 +1,9 @@
 const { loadParticlesPlugin } = require("@tsparticles/webpack-plugin");
 const version = require("./package.json").version;
 
-module.exports = loadParticlesPlugin({ moduleName: "namedColor", pluginName: "Named Color", version, dir: __dirname });
+module.exports = loadParticlesPlugin({
+  moduleName: "namedColor",
+  pluginName: "Named Color",
+  version,
+  dir: __dirname,
+});
