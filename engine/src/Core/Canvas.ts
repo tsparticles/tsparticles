1b64a78882bad2a1ddc17e4eb2bb3db67125effa
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2026-03-24 13:05:12 +0100

    fix: small refactoring fixes

--- engine/src/Core/Canvas.ts
+++ engine/src/Core/Canvas.ts
@@ -597,11 +597,13 @@ export class Canvas {
     this.initBackground();
 
     this._safeMutationObserver(obs => {
-      if (!this.element || !(this.element instanceof Node)) {
+      const element = this.domElement;
+
+      if (!element || !(element instanceof Node)) {
         return;
       }
 
-      obs.observe(this.element, { attributes: true });
+      obs.observe(element, { attributes: true });
     });
   }
 
