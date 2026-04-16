d518b442e5e764ffaed08e124bedbfe4a7d9bb48
Matteo Bruni <176620+matteobruni@users.noreply.github.com>
2026-04-16 10:19:45 +0200

    build: added confetti explosions config

--- utils/configs/src/c/index.ts
+++ utils/configs/src/c/index.ts
@@ -7,6 +7,7 @@ import collisionsAbsorb from "./collisionsAbsorb.js";
 import collisionsBounce from "./collisionsBounce.js";
 import collisionsDestroy from "./collisionsDestroy.js";
 import colorAnimation from "./colorAnimation.js";
+import confettiExplosions from "./confettiExplosions.js";
 import connect from "./connect.js";
 import curlNoise from "./curlNoise.js";
 
@@ -20,6 +21,7 @@ export default {
   collisionsBounce,
   collisionsDestroy,
   colorAnimation,
+  confettiExplosions,
   connect,
   curlNoise,
 };
